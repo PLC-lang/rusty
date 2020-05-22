fe927de35a7be5ceb78e4a27666fd1e55c788850
Mathias Rieder <mathias.rieder@gmail.com>
2020-05-22 16:32:01 +0200

    add -d option for ./podman_watch.sh
    to run with RUST_BACKTRACES=1

--- podman_watch.sh
+++ podman_watch.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-podman run -it -v ./:/usr/local/src -w /usr/local/src rust-llvm sh -c ./cargo_watch.sh
+
+if [ ! -z "$1" ]  && [ $1 == '-d' ] 
+then
+	podman run -it -v ./:/usr/local/src -w /usr/local/src rust-llvm bash -c 'export RUST_BACKTRACE=1 && ./cargo_watch.sh'
+else
+	podman run -it -v ./:/usr/local/src -w /usr/local/src rust-llvm bash -c ./cargo_watch.sh
+fi
