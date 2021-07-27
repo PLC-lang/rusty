10185dcb086289a82dea8fadb0269bf49e4966a6
ghha <Ghaith.HACHEM@bachmann.info>
2021-07-27 13:34:08 +0200

    Add more POUs in the test

--- src/parser/tests/container_parser_tests.rs
+++ src/parser/tests/container_parser_tests.rs
@@ -59,20 +59,26 @@ fn actions_with_no_container_have_unkown_container() {
 
 #[test]
 fn actions_with_no_container_inherits_previous_pou() {
-    let lexer = lex("PROGRAM foo END_PROGRAM ACTIONS ACTION bar END_ACTION END_ACTIONS");
+    let lexer = lex(
+        "PROGRAM buz END_PROGRAM PROGRAM foo END_PROGRAM ACTIONS ACTION bar END_ACTION END_ACTIONS",
+    );
     let (result, diagnostic) = parse(lexer).unwrap();
     let prg = &result.implementations[0];
+    assert_eq!(prg.name, "buz");
+    assert_eq!(prg.type_name, "buz");
+
+    let prg = &result.implementations[1];
     assert_eq!(prg.name, "foo");
     assert_eq!(prg.type_name, "foo");
 
-    let prg = &result.implementations[1];
+    let prg = &result.implementations[2];
     assert_eq!(prg.name, "foo.bar");
     assert_eq!(prg.type_name, "foo");
 
     //Expect a diagnostic
     assert_eq!(
         diagnostic,
-        [Diagnostic::missing_action_container((32..38).into())]
+        [Diagnostic::missing_action_container((56..62).into())]
     );
 }
 
