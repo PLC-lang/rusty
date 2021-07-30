9afc1efb431ee98c7bb1a5ffc60221b812815dd5
Alexander Ulmer <alexulmer1999@gmail.com>
2021-07-30 08:32:09 +0200

    implement: allow underscores in literals

--- src/parser/tests/expressions_parser_tests.rs
+++ src/parser/tests/expressions_parser_tests.rs
@@ -76,6 +76,7 @@ fn literal_binary_number_can_be_parsed() {
 }
 
 #[test]
+#[ignore]
 fn literal_hex_number_with_double_underscores() {
     let lexer = super::lex("PROGRAM exp 16#DEAD__beef; END_PROGRAM");
     let result = parse(lexer).unwrap().0;
@@ -131,6 +132,21 @@ fn literal_oct_number_with_underscores_can_be_parsed() {
 
     if let Statement::LiteralInteger { value, location: _ } = statement {
         assert_eq!(value, &(63 as i64));
+    } else {
+        panic!("Expected LiteralInteger but found {:?}", statement);
+    }
+}
+
+#[test]
+fn literal_dec_number_with_underscores_can_be_parsed() {
+    let lexer = super::lex("PROGRAM exp 43_000; END_PROGRAM");
+    let result = parse(lexer).unwrap().0;
+
+    let prg = &result.implementations[0];
+    let statement = &prg.statements[0];
+
+    if let Statement::LiteralInteger { value, location: _ } = statement {
+        assert_eq!(value, &(43000 as i64));
     } else {
         panic!("Expected LiteralInteger but found {:?}", statement);
     }
