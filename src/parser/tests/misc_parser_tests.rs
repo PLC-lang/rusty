7ed05fc212fc533f3398bb56211664ea5f498f36
Mathias Rieder <mathias.rieder@gmail.com>
2021-07-31 20:05:22 +0000

    improve test coverage for Statements impl

--- src/parser/tests/misc_parser_tests.rs
+++ src/parser/tests/misc_parser_tests.rs
@@ -1,9 +1,13 @@
 use core::panic;
+use std::ops::Range;
 
 // Copyright (c) 2020 Ghaith Hachem and Mathias Rieder
 use crate::{
     ast::*,
-    parser::{parse, tests::lex},
+    parser::{
+        parse,
+        tests::{empty_stmt, lex},
+    },
 };
 use pretty_assertions::*;
 
@@ -399,4 +403,571 @@ fn ids_are_assigned_to_case_statements() {
 
         _ => panic!("invalid statement"),
     }
+}
+
+#[test]
+fn id_implementation_for_all_statements() {
+    assert_eq!(
+        Statement::ArrayAccess {
+            access: Box::new(empty_stmt()),
+            reference: Box::new(empty_stmt()),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::Assignment {
+            left: Box::new(empty_stmt()),
+            right: Box::new(empty_stmt()),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::BinaryExpression {
+            left: Box::new(empty_stmt()),
+            right: Box::new(empty_stmt()),
+            operator: Operator::And,
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::BinaryExpression {
+            left: Box::new(empty_stmt()),
+            right: Box::new(empty_stmt()),
+            operator: Operator::And,
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::CallStatement {
+            operator: Box::new(empty_stmt()),
+            parameters: Box::new(None),
+            id: 7,
+            location: (1..5).into()
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::CaseCondition {
+            condition: Box::new(empty_stmt()),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::CaseStatement {
+            selector: Box::new(empty_stmt()),
+            case_blocks: vec![],
+            else_block: vec![],
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::EmptyStatement {
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::ExpressionList {
+            expressions: vec![],
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::ForLoopStatement {
+            body: vec![],
+            by_step: None,
+            counter: Box::new(empty_stmt()),
+            end: Box::new(empty_stmt()),
+            start: Box::new(empty_stmt()),
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::IfStatement {
+            blocks: vec![],
+            else_block: vec![],
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::LiteralArray {
+            elements: None,
+            location: (1..5).into(),
+            id: 7,
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::LiteralBool {
+            value: true,
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::LiteralInteger {
+            value: 7,
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::LiteralDate {
+            day: 0,
+            month: 0,
+            year: 0,
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::LiteralDateAndTime {
+            day: 0,
+            month: 0,
+            year: 0,
+            hour: 0,
+            milli: 0,
+            min: 0,
+            sec: 0,
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::LiteralReal {
+            value: "2.3".to_string(),
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::LiteralString {
+            is_wide: false,
+            value: "2.3".to_string(),
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::LiteralTime {
+            day: 0.0,
+            hour: 0.0,
+            milli: 0.0,
+            min: 0.0,
+            sec: 0.0,
+            micro: 0.0,
+            nano: 0,
+            negative: false,
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::LiteralTimeOfDay {
+            hour: 0,
+            min: 0,
+            sec: 0,
+            milli: 0,
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::MultipliedStatement {
+            element: Box::new(empty_stmt()),
+            multiplier: 9,
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::OutputAssignment {
+            left: Box::new(empty_stmt()),
+            right: Box::new(empty_stmt()),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::QualifiedReference {
+            elements: vec![],
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::RangeStatement {
+            start: Box::new(empty_stmt()),
+            end: Box::new(empty_stmt()),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::Reference {
+            name: "ab".to_string(),
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::RepeatLoopStatement {
+            body: vec![],
+            condition: Box::new(empty_stmt()),
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::UnaryExpression {
+            operator: Operator::Minus,
+            value: Box::new(empty_stmt()),
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+    assert_eq!(
+        Statement::WhileLoopStatement {
+            body: vec![],
+            condition: Box::new(empty_stmt()),
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_id(),
+        7
+    );
+}
+
+fn at(location: Range<usize>) -> Statement {
+    Statement::EmptyStatement {
+        id: 7,
+        location: location.into(),
+    }
+}
+
+#[test]
+fn location_implementation_for_all_statements() {
+    assert_eq!(
+        Statement::ArrayAccess {
+            reference: Box::new(at(0..1)),
+            access: Box::new(at(2..4)),
+            id: 7
+        }
+        .get_location(),
+        (0..4).into()
+    );
+    assert_eq!(
+        Statement::Assignment {
+            left: Box::new(at(0..2)),
+            right: Box::new(at(3..8)),
+            id: 7
+        }
+        .get_location(),
+        (0..8).into()
+    );
+    assert_eq!(
+        Statement::BinaryExpression {
+            left: Box::new(at(0..2)),
+            right: Box::new(at(3..8)),
+            operator: Operator::And,
+            id: 7
+        }
+        .get_location(),
+        (0..8).into()
+    );
+    assert_eq!(
+        Statement::CallStatement {
+            operator: Box::new(empty_stmt()),
+            parameters: Box::new(None),
+            id: 7,
+            location: (1..5).into()
+        }
+        .get_location(),
+        (1..5).into()
+    );
+    assert_eq!(
+        Statement::CaseCondition {
+            condition: Box::new(at(2..4)),
+            id: 7
+        }
+        .get_location(),
+        (2..4).into()
+    );
+    assert_eq!(
+        Statement::CaseStatement {
+            selector: Box::new(empty_stmt()),
+            case_blocks: vec![],
+            else_block: vec![],
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_location(),
+        (1..5).into()
+    );
+    assert_eq!(
+        Statement::EmptyStatement {
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_location(),
+        (1..5).into()
+    );
+    assert_eq!(
+        Statement::ExpressionList {
+            expressions: vec![at(0..3), at(4..8)],
+            id: 7
+        }
+        .get_location(),
+        (0..8).into()
+    );
+    assert_eq!(
+        Statement::ForLoopStatement {
+            body: vec![],
+            by_step: None,
+            counter: Box::new(empty_stmt()),
+            end: Box::new(empty_stmt()),
+            start: Box::new(empty_stmt()),
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_location(),
+        (1..5).into()
+    );
+    assert_eq!(
+        Statement::IfStatement {
+            blocks: vec![],
+            else_block: vec![],
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_location(),
+        (1..5).into()
+    );
+    assert_eq!(
+        Statement::LiteralArray {
+            elements: None,
+            location: (1..5).into(),
+            id: 7,
+        }
+        .get_location(),
+        (1..5).into()
+    );
+    assert_eq!(
+        Statement::LiteralBool {
+            value: true,
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_location(),
+        (1..5).into()
+    );
+    assert_eq!(
+        Statement::LiteralInteger {
+            value: 7,
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_location(),
+        (1..5).into()
+    );
+    assert_eq!(
+        Statement::LiteralDate {
+            day: 0,
+            month: 0,
+            year: 0,
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_location(),
+        (1..5).into()
+    );
+    assert_eq!(
+        Statement::LiteralDateAndTime {
+            day: 0,
+            month: 0,
+            year: 0,
+            hour: 0,
+            milli: 0,
+            min: 0,
+            sec: 0,
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_location(),
+        (1..5).into()
+    );
+    assert_eq!(
+        Statement::LiteralReal {
+            value: "2.3".to_string(),
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_location(),
+        (1..5).into()
+    );
+    assert_eq!(
+        Statement::LiteralString {
+            is_wide: false,
+            value: "2.3".to_string(),
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_location(),
+        (1..5).into()
+    );
+    assert_eq!(
+        Statement::LiteralTime {
+            day: 0.0,
+            hour: 0.0,
+            milli: 0.0,
+            min: 0.0,
+            sec: 0.0,
+            micro: 0.0,
+            nano: 0,
+            negative: false,
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_location(),
+        (1..5).into()
+    );
+    assert_eq!(
+        Statement::LiteralTimeOfDay {
+            hour: 0,
+            min: 0,
+            sec: 0,
+            milli: 0,
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_location(),
+        (1..5).into()
+    );
+    assert_eq!(
+        Statement::MultipliedStatement {
+            element: Box::new(empty_stmt()),
+            multiplier: 9,
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_location(),
+        (1..5).into()
+    );
+    assert_eq!(
+        Statement::OutputAssignment {
+            left: Box::new(at(0..3)),
+            right: Box::new(at(4..9)),
+            id: 7
+        }
+        .get_location(),
+        (0..9).into()
+    );
+    assert_eq!(
+        Statement::QualifiedReference {
+            elements: vec![at(0..3), at(4..5)],
+            id: 7
+        }
+        .get_location(),
+        (0..5).into()
+    );
+    assert_eq!(
+        Statement::RangeStatement {
+            start: Box::new(at(0..3)),
+            end: Box::new(at(6..9)),
+            id: 7
+        }
+        .get_location(),
+        (0..9).into()
+    );
+    assert_eq!(
+        Statement::Reference {
+            name: "ab".to_string(),
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_location(),
+        (1..5).into()
+    );
+    assert_eq!(
+        Statement::RepeatLoopStatement {
+            body: vec![],
+            condition: Box::new(empty_stmt()),
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_location(),
+        (1..5).into()
+    );
+    assert_eq!(
+        Statement::UnaryExpression {
+            operator: Operator::Minus,
+            value: Box::new(empty_stmt()),
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_location(),
+        (1..5).into()
+    );
+    assert_eq!(
+        Statement::WhileLoopStatement {
+            body: vec![],
+            condition: Box::new(empty_stmt()),
+            location: (1..5).into(),
+            id: 7
+        }
+        .get_location(),
+        (1..5).into()
+    );
 }
