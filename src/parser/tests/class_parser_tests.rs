6a583548963b8b3c6f2c83273f834e60b1add87e
Alexander Ulmer <alexulmer1999@gmail.com>
2021-08-06 09:58:41 +0200

    add tests for method variable blocks

--- src/parser/tests/class_parser_tests.rs
+++ src/parser/tests/class_parser_tests.rs
@@ -72,6 +72,24 @@ fn method_can_be_parsed() {
 }
 
 #[test]
+fn two_methods_can_be_parsed() {
+    let lexer =
+        lex("CLASS MyClass METHOD INTERNAL testMethod2 END_METHOD METHOD otherMethod VAR_TEMP END_VAR END_METHOD END_CLASS");
+    let unit = parse(lexer).0;
+
+    let class = unit.classes.first().unwrap();
+    assert_eq!(class.methods.len(), 2);
+
+    let method1 = &class.methods[0];
+    assert_eq!(method1.name, "testMethod2");
+    assert_eq!(method1.access, AccessModifier::Internal);
+
+    let method2 = &class.methods[1];
+    assert_eq!(method2.name, "otherMethod");
+    assert_eq!(method2.access, AccessModifier::Protected);
+}
+
+#[test]
 fn method_with_return_type_can_be_parsed() {
     let lexer = lex(
         "CLASS MyClass METHOD PRIVATE ABSTRACT OVERRIDE testMethod3 : SINT END_METHOD END_CLASS",
@@ -92,9 +110,7 @@ fn method_with_return_type_can_be_parsed() {
 
 #[test]
 fn class_with_var_default_block() {
-    let lexer = lex(
-        "CLASS MyClass VAR END_VAR END_CLASS",
-    );
+    let lexer = lex("CLASS MyClass VAR END_VAR END_CLASS");
     let unit = parse(lexer).0;
 
     let class = unit.classes.first().unwrap();
@@ -111,9 +127,7 @@ fn class_with_var_default_block() {
 
 #[test]
 fn class_with_var_non_retain_block() {
-    let lexer = lex(
-        "CLASS MyClass VAR CONSTANT NON_RETAIN PUBLIC END_VAR END_CLASS",
-    );
+    let lexer = lex("CLASS MyClass VAR CONSTANT NON_RETAIN PUBLIC END_VAR END_CLASS");
     let unit = parse(lexer).0;
 
     let class = unit.classes.first().unwrap();
@@ -130,9 +144,7 @@ fn class_with_var_non_retain_block() {
 
 #[test]
 fn class_with_var_retain_block() {
-    let lexer = lex(
-        "CLASS MyClass VAR RETAIN INTERNAL END_VAR END_CLASS",
-    );
+    let lexer = lex("CLASS MyClass VAR RETAIN INTERNAL END_VAR END_CLASS");
     let unit = parse(lexer).0;
 
     let class = unit.classes.first().unwrap();
@@ -145,4 +157,48 @@ fn class_with_var_retain_block() {
     assert_eq!(vblock.constant, false);
     assert_eq!(vblock.access, AccessModifier::Internal);
     assert_eq!(vblock.variable_block_type, VariableBlockType::Local);
+}
+
+#[test]
+fn method_with_var_block() {
+    let lexer = lex("CLASS MyClass METHOD testMethod3 VAR END_VAR END_METHOD END_CLASS");
+    let unit = parse(lexer).0;
+
+    let class = unit.classes.first().unwrap();
+    assert_eq!(class.methods.len(), 1);
+
+    let method = class.methods.first().unwrap();
+    let vblock = method.variable_blocks.first().unwrap();
+
+    assert_eq!(vblock.retain, false);
+    assert_eq!(vblock.constant, false);
+    assert_eq!(vblock.access, AccessModifier::Protected);
+    assert_eq!(vblock.variable_block_type, VariableBlockType::Local);
+}
+
+#[test]
+fn method_with_var_inout_blocks() {
+    let lexer = lex(
+        "CLASS MyClass METHOD testMethod3 VAR_INPUT CONSTANT x:SINT := 3; END_VAR VAR_IN_OUT END_VAR VAR_OUTPUT END_VAR END_METHOD END_CLASS",
+    );
+    let unit = parse(lexer).0;
+
+    let class = unit.classes.first().unwrap();
+    assert_eq!(class.methods.len(), 1);
+
+    let method = class.methods.first().unwrap();
+
+    assert_eq!(method.variable_blocks.len(), 3);
+    let vblock1 = &method.variable_blocks[0];
+    let vblock2 = &method.variable_blocks[1];
+    let vblock3 = &method.variable_blocks[2];
+
+    assert_eq!(vblock1.constant, true);
+    assert_eq!(vblock1.variable_block_type, VariableBlockType::Input);
+
+    assert_eq!(vblock2.constant, false);
+    assert_eq!(vblock2.variable_block_type, VariableBlockType::InOut);
+
+    assert_eq!(vblock3.constant, false);
+    assert_eq!(vblock3.variable_block_type, VariableBlockType::Output);
 }
--- src/parser/tests/parse_errors/parse_error_containers_tests.rs
+++ src/parser/tests/parse_errors/parse_error_containers_tests.rs
@@ -1,5 +1,10 @@
 // Copyright (c) 2020 Ghaith Hachem and Mathias Rieder
-use crate::{Diagnostic, ast::*, lexer::Token, parser::{parse, tests::lex}};
+use crate::{
+    ast::*,
+    lexer::Token,
+    parser::{parse, tests::lex},
+    Diagnostic,
+};
 use pretty_assertions::*;
 
 /*
