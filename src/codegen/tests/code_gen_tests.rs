8682fb863cc9afee6234f57262570a38da3b8bc5
Alexander Ulmer <alexulmer1999@gmail.com>
2021-08-12 09:51:45 +0200

    add parser and codegen tests for RETURN

--- src/codegen/tests/code_gen_tests.rs
+++ src/codegen/tests/code_gen_tests.rs
@@ -3798,6 +3798,47 @@ fn nested_array_access() {
 }
 
 #[test]
+fn returning_early_in_function() {
+    let result = codegen!(
+        "
+        FUNCTION smaller_than_ten: INT
+          VAR_INPUT n : SINT; END_VAR
+          IF n < 10 THEN
+                  RETURN;
+          END_IF;
+        END_FUNCTION
+        "
+    );
+
+    let expected = r#"; ModuleID = 'main'
+source_filename = "main"
+
+%smaller_than_ten_interface = type { i8 }
+
+define i16 @smaller_than_ten(%smaller_than_ten_interface* %0) {
+entry:
+  %n = getelementptr inbounds %smaller_than_ten_interface, %smaller_than_ten_interface* %0, i32 0, i32 0
+  %smaller_than_ten = alloca i16, align 2
+  %load_n = load i8, i8* %n, align 1
+  %1 = sext i8 %load_n to i32
+  %tmpVar = icmp slt i32 %1, 10
+  br i1 %tmpVar, label %condition_body, label %continue
+
+condition_body:                                   ; preds = %entry
+  %smaller_than_ten_ret = load i16, i16* %smaller_than_ten, align 2
+  ret i16 %smaller_than_ten_ret
+  br label %continue
+
+continue:                                         ; preds = %condition_body, %entry
+  %smaller_than_ten_ret1 = load i16, i16* %smaller_than_ten, align 2
+  ret i16 %smaller_than_ten_ret1
+}
+"#;
+
+    assert_eq!(result, expected);
+}
+
+#[test]
 fn accessing_nested_array_in_struct() {
     let result = codegen!(
         "
