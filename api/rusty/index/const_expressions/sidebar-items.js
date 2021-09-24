85d1e141e081f736b044e60b8a353b39422dea4a
ghaith <ghaith@users.noreply.github.com>
2021-09-24 09:25:37 +0000

    deploy: 6f3e63c92e82535cf1ef357124537ecfc5a21ac3

--- api/rusty/index/const_expressions/sidebar-items.js
+++ api/rusty/index/const_expressions/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["ConstExpressions",""]],"type":[["ConstId",""]]});
+initSidebarItems({"enum":[["ConstExpression","constant expressions registered here are wrapped behind this enum to indicate whether this expression was already (potentially) resolved or not, or if a resolving failed."]],"struct":[["ConstExpressions",""],["IntoStatementIter",""]],"type":[["ConstId",""]]});
