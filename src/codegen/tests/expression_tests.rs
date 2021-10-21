6fa345b97f42a88918b784668a58b9ddb7d86407
Ghaith <ghaith.hachem@bachmann.info>
2021-10-21 09:42:42 +0200

    Fix compile error due to merge

--- src/codegen/tests/expression_tests.rs
+++ src/codegen/tests/expression_tests.rs
@@ -1,25 +1,25 @@
 // Copyright (c) 2020 Ghaith Hachem and Mathias Rieder
-use crate::codegen;
+use crate::test_utils::tests::codegen;
 
 #[test]
 fn pointers_in_function_return() {
-    let result = codegen!(
+    let result = codegen(
         r#"FUNCTION func : REF_TO INT
-        END_FUNCTION"#
+        END_FUNCTION"#,
     );
     insta::assert_snapshot!(result);
 }
 
 #[test]
 fn structs_in_function_return() {
-    let result = codegen!(
+    let result = codegen(
         r#"
         TYPE myStruct : STRUCT
             x : INT;
             END_STRUCT
         END_TYPE
         FUNCTION func : myStruct
-        END_FUNCTION"#
+        END_FUNCTION"#,
     );
     insta::assert_snapshot!(result);
 }
