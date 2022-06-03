3db60dcb43a8f82d1e02b15f3631aa7d0d10374d
Mathias Rieder <mathias.rieder@gmail.com>
2022-06-03 09:50:38 +0200

    improved validation of FUNCs without return types (#498)
    Functions with missing return types used to report a problem on the
    whole POU (declaration and body). I added an additional location field
    to the POU (AST) that stores the location of the POU's name so we can
    report the error more nicely where it actually happened.
    
    also activated dead test (pou_validation_tests)

--- src/validation/tests.rs
+++ src/validation/tests.rs
@@ -1,10 +1,9 @@
 // Copyright (c) 2020 Ghaith Hachem and Mathias Rieder
 mod array_access_validation_test;
 mod bitaccess_validation_test;
-mod literals_validation_tests;
-mod reference_resolve_tests;
-mod variable_validation_tests;
-
-mod statement_validation_tests;
-
 mod generic_validation_tests;
+mod literals_validation_tests;
+mod pou_validation_tests;
+mod reference_resolve_tests;
+mod statement_validation_tests;
+mod variable_validation_tests;
