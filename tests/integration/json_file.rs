0281b429436d25389e1e2df559f27c78ce3c5ef1
Flavio Schuricht <flavio.schuricht@bachmann.info>
2022-08-01 11:56:05 +0000

    tests improved

--- tests/integration/json_file.rs
+++ tests/integration/json_file.rs
@@ -3,8 +3,6 @@ use crate::diagnostics::Diagnostic;
 use crate::get_test_file;
 use rusty::build_with_subcommand;
 use std::env::temp_dir;
-use std::fs::File;
-use std::io::Read;
 use std::path::Path;
 
 macro_rules! vec_of_strings {
@@ -28,39 +26,8 @@ fn build_to_temp() -> Result<(), Diagnostic> {
         std::process::exit(1);
     }
 
-    let mut new_build = File::open(&format!("{}/proj.so", dir.display()))?;
-    let mut old_build = File::open(&get_test_file("json/test_build/proj.so"))?;
-
     assert!(Path::new(&format!("{}/libcopy.so", dir.display())).is_file());
     assert!(Path::new(&format!("{}/proj.so", dir.display())).is_file());
-    assert!(diff_files(&mut new_build, &mut old_build));
 
     Ok(())
-}
-
-pub fn diff_files(f1: &mut File, f2: &mut File) -> bool {
-    let buff1 = &mut [0; 2048];
-    let buff2 = &mut [0; 2048];
-
-    loop {
-        match f1.read(buff1) {
-            Err(_) => return false,
-            Ok(f1_read_len) => match f2.read(buff2) {
-                Err(_) => return false,
-                Ok(f2_read_len) => {
-                    if f1_read_len != f2_read_len {
-                        println!("uneven found");
-
-                        return false;
-                    }
-                    if f1_read_len == 0 {
-                        return true;
-                    }
-                    if buff1[0..f1_read_len] != buff2[0..f2_read_len] {
-                        return false;
-                    }
-                }
-            },
-        }
-    }
 }
