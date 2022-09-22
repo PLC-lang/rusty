85493ee67762257517d84b73b8ba0cd9fa1d1b3b
Nikola Milic <Nikola.MILIC@bachmann.info>
2022-09-22 08:58:29 +0200

    #570 resolver handle casting int to pointer size

--- src/codegen/llvm_typesystem.rs
+++ src/codegen/llvm_typesystem.rs
@@ -94,7 +94,7 @@ pub fn promote_value_if_needed<'ctx>(
     }
 }
 
-pub fn create_llvm_extend_int_value<'a>(
+fn create_llvm_extend_int_value<'a>(
     builder: &Builder<'a>,
     lvalue: IntValue<'a>,
     ltype: &DataTypeInformation,
@@ -108,42 +108,6 @@ pub fn create_llvm_extend_int_value<'a>(
             builder.build_int_z_extend_or_bit_cast(lvalue, target_type, "")
         }
         _ => unreachable!(),
-    }
-}
-
-///
-/// expand / truncate the given `int_value` to the given `target_size` if neccessary
-/// It returns the original `int_value` if no cast is necessary
-/// - `llvm` the llvm utilities to use for code-generation
-/// - `int_value` the value to (maybe) cast
-/// - `value_type` the current type of the given value
-/// - `target_size` the expected size
-///
-pub fn cast_int_to_target_size_if_needed<'a>(
-    llvm: &Llvm<'a>,
-    int_value: IntValue<'a>,
-    value_type: &DataTypeInformation,
-    target_size: &u32,
-) -> Result<IntValue<'a>, Diagnostic> {
-    match int_value.get_type().get_bit_width() {
-        s if s < *target_size => {
-            // extend
-            Ok(create_llvm_extend_int_value(
-                &llvm.builder,
-                int_value,
-                value_type,
-                get_llvm_int_type(llvm.context, *target_size, "Integer")?,
-            ))
-        }
-        s if s > *target_size => {
-            // truncate
-            Ok(llvm.builder.build_int_truncate_or_bit_cast(
-                int_value,
-                get_llvm_int_type(llvm.context, *target_size, "Integer")?,
-                "",
-            ))
-        }
-        _ => Ok(int_value),
     }
 }
 
