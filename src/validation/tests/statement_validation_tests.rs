c597dfe5e67a0552121f48321ecc6c6b29f0eced
Michael HASELBERGER <Michael.HASELBERGER@bachmann.info>
2022-10-06 16:09:11 +0200

    fixes syntax error in validator for numeric subrange compare statements
    this happened because the validator was trying to find a compare function for the subrange name, not its referenced type.

--- src/validation/tests/statement_validation_tests.rs
+++ src/validation/tests/statement_validation_tests.rs
@@ -492,3 +492,58 @@ fn switch_case_duplicate_integer() {
         ]
     );
 }
+
+#[test]
+fn subrange_compare_function_causes_no_error() {
+    // GIVEN switch case with duplicate constant conditions
+    // WHEN it is validated
+    let diagnostics = parse_and_validate(
+        r#"
+        PROGRAM main
+        VAR 
+            a, b, c, d, e, f : BOOL;
+        END_VAR      
+        VAR_TEMP
+            x,y : INT(0..500);
+        END_VAR
+            a := x < y;
+            b := x = y;
+            c := x = 3;
+            d := y = 500;
+            e := x >= 0 AND x <= 500;
+            f := x < 0 OR x > 500;
+        END_PROGRAM
+        "#,
+    );
+
+    // THEN the non constant variables are reported
+    assert_eq!(diagnostics, vec![]);
+}
+
+#[test]
+fn aliased_subrange_compare_function_causes_no_error() {
+    // GIVEN switch case with duplicate constant conditions
+    // WHEN it is validated
+    let diagnostics = parse_and_validate(
+        r#"
+        TYPE MyInt: INT(0..500); END_TYPE
+        PROGRAM main
+        VAR 
+            a, b, c, d, e, f : BOOL;
+        END_VAR      
+        VAR_TEMP
+            x,y : MyInt;
+        END_VAR
+            a := x < y;
+            b := x = y;
+            c := x = 3;
+            d := y = 500;
+            e := x >= 0 AND x <= 500;
+            f := x < 0 OR x > 500;
+        END_PROGRAM
+        "#,
+    );
+
+    // THEN the non constant variables are reported
+    assert_eq!(diagnostics, vec![]);
+}
