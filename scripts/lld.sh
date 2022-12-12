da9da4e873459001abf686caba3b1f26373145f6
Volkan Sagcan <volkan.sagcan@bachmann.info>
2022-12-12 12:25:34 +0000

    Update `lld.sh` and symlink `ld.lld` in Dockerfile

--- scripts/lld.sh
+++ scripts/lld.sh
@@ -1,23 +1,35 @@
 #!/usr/bin/env sh
 
+LD_LLD_WITH_VERSION=$(command -v ld.lld-$1)
+LD_LLD_WITHOUT_VERSION=$(command -v ld.lld)
+
 if [ -z $1 ]; then
+    echo "No version specified!"
     echo "Usage:   $0 <LLD version>"
     echo "Example: $0 13"
     exit 1
 fi
 
-# Check if the specified LLD version is installed
-if ! [ -x "$(command -v ld.lld-$1)" ]; then
-    echo "LLD-$1 was not found!"
+# Check if the specified `lld` version is present on the system
+if [ -z $LD_LLD_WITH_VERSION ]; then
+    echo "Could not find ld.lld-$1 binary, aborting"
     exit 1
 fi
 
-# Symlink `lld` to `ld.lld` which is needed by `.cargo/config.toml`
-sudo ln -svf $(command -v ld.lld-$1) /usr/bin/ld.lld
+# Check if `ld.lld` is present on the system
+if [ -z $LD_LLD_WITHOUT_VERSION ]; then 
+    # Not present, create a `/usr/bin/ld.lld -> /usr/bin/ld.lld-$1` symlink
+    sudo ln -sf $LD_LLD_WITH_VERSION /usr/bin/ld.lld
+else 
+    # Present, do nothing because we don't want to modifiy system-wide configurations
+    echo "Note: Using already present $LD_LLD_WITHOUT_VERSION binary"
+    echo "      Make sure $LD_LLD_WITHOUT_VERSION matches with RuSTys LLVM version"
+fi
 
-# Create the `.cargo/config.toml` file
+# Create `config.toml` to make `lld` the default linker for RuSTy
 mkdir -p .cargo
 echo -n '[build]
 rustflags = ["-C", "link-arg=-fuse-ld=lld"]' > .cargo/config.toml
 
+# Makes sure new builds with lld succeed
 cargo clean
