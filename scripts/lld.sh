fbba819e7233cdc45a1d6ec141979fd179b60172
Volkan Sagcan <volkan.sagcan@bachmann.info>
2022-12-12 13:08:16 +0000

    Remove sudo statement, replace with ~/.local/bin

--- scripts/lld.sh
+++ scripts/lld.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+TEXT_BOLD=$(tput bold)
+TEXT_NORMAL=$(tput sgr0)
+
 LD_LLD_WITH_VERSION=$(command -v ld.lld-$1)
 LD_LLD_WITHOUT_VERSION=$(command -v ld.lld)
 
@@ -12,14 +15,19 @@ fi
 
 # Check if the specified `lld` version is present on the system
 if [ -z $LD_LLD_WITH_VERSION ]; then
-    echo "Could not find ld.lld-$1 binary, aborting"
+    echo "Could not find ld.lld-$1, make sure lld is installed"
     exit 1
 fi
 
 # Check if `ld.lld` is present on the system
 if [ -z $LD_LLD_WITHOUT_VERSION ]; then 
-    # Not present, create a `/usr/bin/ld.lld -> /usr/bin/ld.lld-$1` symlink
-    sudo ln -sf $LD_LLD_WITH_VERSION /usr/bin/ld.lld
+    # Not present, create a symlink into ~/.local/bin
+    mkdir -p $HOME/.local/bin
+    ln -svf $LD_LLD_WITH_VERSION $HOME/.local/bin/ld.lld
+
+    echo "\nYou might need to update your \$PATH environment variable for cargo to recognize lld, like so:"
+    echo "${TEXT_BOLD}echo 'export PATH=\$HOME/.local/bin:\$PATH' >> $HOME/.bashrc${TEXT_NORMAL}"
+
 else 
     # Present, do nothing because we don't want to modifiy system-wide configurations
     echo "Note: Using already present $LD_LLD_WITHOUT_VERSION binary"
