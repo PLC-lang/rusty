d424f8ab77f7433533160f0b53a0a574c742d6d6
99NIMI <99NIMI@users.noreply.github.com>
2023-02-01 10:09:18 +0000

    deploy: dbdf32517d778fa7bcbf036664ad2fc183708179

--- api/inkwell/context/sidebar-items.js
+++ api/inkwell/context/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Context","A `Context` is a container for all LLVM entities including `Module`s."],["ContextRef","A `ContextRef` is a smart pointer allowing borrowed access to a type’s `Context`."]]});
+initSidebarItems({"struct":[["Context","A `Context` is a container for all LLVM entities including `Module`s."],["ContextRef","A `ContextRef` is a smart pointer allowing borrowed access to a type’s `Context`."]],"trait":[["AsContextRef","This trait abstracts an LLVM `Context` type and should be implemented with caution."]]});
