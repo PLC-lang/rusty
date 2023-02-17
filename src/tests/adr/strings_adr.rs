aa3fae67b7a8627fb4a1e30694636fa7a1eab347
Mathias Rieder <mathias.rieder@bachmann.info>
2023-02-17 22:01:34 +0100

    ADR documentation (arrays, strings, structs) (#777)
    * ADR documentation (arrays, strings, structs)
    
    introduce the architecture design records for aggreate datatypes

---
+++ src/tests/adr/strings_adr.rs
@@ -0,0 +1,132 @@
+use crate::test_utils::tests::codegen;
+
+/// # Architecture Design Record: Strings
+/// ST supports two types of Strings: UTF8 and UTF16 Strings. Strings are fixed size and
+/// stored using i8-arrays for utf8 strings and i16-arrays for utf16 strings.
+#[test]
+fn declaring_a_string() {
+    // two string variables
+    let src = r#"
+        VAR_GLOBAL
+            myUtf8  : STRING[20];
+            myUtf16 : WSTRING[20];
+        END_VAR
+        "#;
+
+    // ... are stored as i8/i16 arrays and get initialized to blank (0)
+    insta::assert_snapshot!(codegen(src), @r###"
+    ; ModuleID = 'main'
+    source_filename = "main"
+
+    @myUtf8 = global [21 x i8] zeroinitializer
+    @myUtf16 = global [21 x i16] zeroinitializer
+    "###);
+}
+
+/// rusty treats strings like C-strings (char arrays) so the interoperability
+/// with C-libraries is seamless.
+#[test]
+fn strings_are_terminated_with_0byte() {
+    // two strings with an initial value ...
+    let src = r#"
+        VAR_GLOBAL
+            myUtf8  : STRING[5]  := 'Hello';
+            myUtf16 : WSTRING[5] := "World";
+        END_VAR
+        "#;
+
+    // ... get stored as c-like char arrays with 0-terminators
+    // ... offer one extra entry (length 21 while only 20 were declared) for a terminator
+    insta::assert_snapshot!(codegen(src), @r###"
+    ; ModuleID = 'main'
+    source_filename = "main"
+
+    @myUtf8 = global [6 x i8] c"Hello\00"
+    @myUtf16 = global [6 x i16] [i16 87, i16 111, i16 114, i16 108, i16 100, i16 0]
+    "###);
+}
+
+/// Strings are aggregate types. This means that passing them to functions and assigning them
+/// are expensive operations (when compared to passing ar assigning an INT). Aggregate types
+/// are assigned using memcpy.
+#[test]
+fn assigning_strings() {
+    // two strings get assigned ...
+    let src = r#"
+        PROGRAM prg
+            VAR
+                a,b : STRING[10];
+            END_VAR
+             a := b;
+        END_PROGRAM
+        "#;
+
+    // ... the assignments will be performed as a memcpy
+    insta::assert_snapshot!(codegen(src), @r###"
+    ; ModuleID = 'main'
+    source_filename = "main"
+
+    %prg = type { [11 x i8], [11 x i8] }
+
+    @prg_instance = global %prg zeroinitializer
+
+    define void @prg(%prg* %0) {
+    entry:
+      %a = getelementptr inbounds %prg, %prg* %0, i32 0, i32 0
+      %b = getelementptr inbounds %prg, %prg* %0, i32 0, i32 1
+      %1 = bitcast [11 x i8]* %a to i8*
+      %2 = bitcast [11 x i8]* %b to i8*
+      call void @llvm.memcpy.p0i8.p0i8.i32(i8* align 1 %1, i8* align 1 %2, i32 10, i1 false)
+      ret void
+    }
+
+    ; Function Attrs: argmemonly nofree nounwind willreturn
+    declare void @llvm.memcpy.p0i8.p0i8.i32(i8* noalias nocapture writeonly, i8* noalias nocapture readonly, i32, i1 immarg) #0
+
+    attributes #0 = { argmemonly nofree nounwind willreturn }
+    "###);
+}
+
+/// STRING literals will be generated as global constants, so they can be used as a source of a memcpy
+/// operation
+#[test]
+fn assigning_string_literals() {
+    // two nested struct-variables that get initialized ...
+    let src = r#"
+        PROGRAM prg
+            VAR
+                a,b : STRING[10];
+            END_VAR
+             a := 'hello';
+             b := 'world';
+        END_PROGRAM
+        "#;
+
+    // ... will be initialized directly in the variable's definition
+    insta::assert_snapshot!(codegen(src), @r###"
+    ; ModuleID = 'main'
+    source_filename = "main"
+
+    %prg = type { [11 x i8], [11 x i8] }
+
+    @prg_instance = global %prg zeroinitializer
+    @utf08_literal_0 = unnamed_addr constant [6 x i8] c"hello\00"
+    @utf08_literal_1 = unnamed_addr constant [6 x i8] c"world\00"
+
+    define void @prg(%prg* %0) {
+    entry:
+      %a = getelementptr inbounds %prg, %prg* %0, i32 0, i32 0
+      %b = getelementptr inbounds %prg, %prg* %0, i32 0, i32 1
+      %1 = bitcast [11 x i8]* %a to i8*
+      call void @llvm.memcpy.p0i8.p0i8.i32(i8* align 1 %1, i8* align 1 getelementptr inbounds ([6 x i8], [6 x i8]* @utf08_literal_0, i32 0, i32 0), i32 6, i1 false)
+      %2 = bitcast [11 x i8]* %b to i8*
+      call void @llvm.memcpy.p0i8.p0i8.i32(i8* align 1 %2, i8* align 1 getelementptr inbounds ([6 x i8], [6 x i8]* @utf08_literal_1, i32 0, i32 0), i32 6, i1 false)
+      ret void
+    }
+
+    ; Function Attrs: argmemonly nofree nounwind willreturn
+    declare void @llvm.memcpy.p0i8.p0i8.i32(i8* noalias nocapture writeonly, i8* noalias nocapture readonly, i32, i1 immarg) #0
+
+    attributes #0 = { argmemonly nofree nounwind willreturn }
+    "###);
+}
---
+++ src/tests/adr/structs_adr.rs
@@ -0,0 +1,220 @@
+use crate::test_utils::tests::codegen;
+
+/// # Architecture Design Record: Structs
+/// ST supports C-like structures.
+///
+#[test]
+fn declaring_a_struct() {
+    // a struct type ...
+    let src = r#"
+        TYPE Person:
+        STRUCT
+            firstName   : STRING;
+            lastName    : STRING;
+            yearOfBirth : INT;
+            isLoggedIn  : BOOL;
+        END_STRUCT
+        END_TYPE
+        "#;
+
+    // ... just translates to a llvm struct type
+    insta::assert_snapshot!(codegen(src), @r###"
+    ; ModuleID = 'main'
+    source_filename = "main"
+
+    %Person = type { [81 x i8], [81 x i8], i16, i8 }
+
+    @__Person__init = unnamed_addr constant %Person zeroinitializer
+    "###);
+}
+
+/// Structs can have a default value by initializing its fields.
+#[test]
+fn default_values_of_a_struct() {
+    // The fields of a struct can get default values ...
+    let src = r#"
+        TYPE Person:
+        STRUCT
+            firstName   : STRING[5] := 'Jane';
+            lastName    : STRING[5] := 'Row';
+            yearOfBirth : INT    := 1988;
+            isLoggedIn  : BOOL   := FALSE;
+        END_STRUCT
+        END_TYPE
+
+        VAR_GLOBAL
+            p : Person;
+        END_VAR
+        "#;
+
+    // ... instances of this struct-type will be initialized accordingly
+    insta::assert_snapshot!(codegen(src), @r###"
+    ; ModuleID = 'main'
+    source_filename = "main"
+
+    %Person = type { [6 x i8], [6 x i8], i16, i8 }
+
+    @p = global %Person { [6 x i8] c"Jane\00\00", [6 x i8] c"Row\00\00\00", i16 1988, i8 0 }
+    @__Person__init = unnamed_addr constant %Person { [6 x i8] c"Jane\00\00", [6 x i8] c"Row\00\00\00", i16 1988, i8 0 }
+    "###);
+}
+
+/// a Struct can also be assigned a struct-literal.
+#[test]
+fn initializing_a_struct() {
+    // two nested struct-variables that get initialized ...
+    let src = r#"
+        TYPE Point:
+        STRUCT
+            x, y   : INT;
+        END_STRUCT
+        END_TYPE
+
+        TYPE Rect:
+        STRUCT
+            topLeft, bottomRight   : Point;
+        END_STRUCT
+        END_TYPE
+
+        PROGRAM prg
+            VAR
+                rect1  : Rect := (
+                            topLeft := (x := 1, y := 5),
+                            bottomRight := (x := 10, y := 15));
+                rect2  : Rect := (
+                            topLeft := (x := 4, y := 6),
+                            bottomRight := (x := 16, y := 22));
+            END_VAR
+        END_PROGRAM
+        "#;
+
+    // ... will be initialized directly in the variable's definition
+    insta::assert_snapshot!(codegen(src), @r###"
+    ; ModuleID = 'main'
+    source_filename = "main"
+
+    %prg = type { %Rect, %Rect }
+    %Rect = type { %Point, %Point }
+    %Point = type { i16, i16 }
+
+    @prg_instance = global %prg { %Rect { %Point { i16 1, i16 5 }, %Point { i16 10, i16 15 } }, %Rect { %Point { i16 4, i16 6 }, %Point { i16 16, i16 22 } } }
+    @__Point__init = unnamed_addr constant %Point zeroinitializer
+    @__Rect__init = unnamed_addr constant %Rect zeroinitializer
+    @__prg.rect1__init = unnamed_addr constant %Rect { %Point { i16 1, i16 5 }, %Point { i16 10, i16 15 } }
+    @__prg.rect2__init = unnamed_addr constant %Rect { %Point { i16 4, i16 6 }, %Point { i16 16, i16 22 } }
+
+    define void @prg(%prg* %0) {
+    entry:
+      %rect1 = getelementptr inbounds %prg, %prg* %0, i32 0, i32 0
+      %rect2 = getelementptr inbounds %prg, %prg* %0, i32 0, i32 1
+      ret void
+    }
+    "###);
+}
+
+/// Structs are aggregate types. This means that passing them to functions and assigning them
+/// are expensive operations (when compared to passing ar assigning an INT). Aggregate types like
+/// structs and arrays are assigned using memcpy.
+#[test]
+fn assigning_structs() {
+    // two struct instances that get assigned and passed to a function ...
+    let src = r#"
+        TYPE Point:
+        STRUCT
+            x, y   : INT;
+        END_STRUCT
+        END_TYPE
+
+        PROGRAM prg
+            VAR
+                p1  : Point;
+                p2  : Point;
+            END_VAR
+
+            p1 := p2;
+        END_PROGRAM
+        "#;
+
+    // ... the assignment p1 := p2 will be performed as a memcpy
+    insta::assert_snapshot!(codegen(src), @r###"
+    ; ModuleID = 'main'
+    source_filename = "main"
+
+    %prg = type { %Point, %Point }
+    %Point = type { i16, i16 }
+
+    @prg_instance = global %prg zeroinitializer
+    @__Point__init = unnamed_addr constant %Point zeroinitializer
+
+    define void @prg(%prg* %0) {
+    entry:
+      %p1 = getelementptr inbounds %prg, %prg* %0, i32 0, i32 0
+      %p2 = getelementptr inbounds %prg, %prg* %0, i32 0, i32 1
+      %1 = bitcast %Point* %p1 to i8*
+      %2 = bitcast %Point* %p2 to i8*
+      call void @llvm.memcpy.p0i8.p0i8.i64(i8* align 1 %1, i8* align 1 %2, i64 ptrtoint (%Point* getelementptr (%Point, %Point* null, i32 1) to i64), i1 false)
+      ret void
+    }
+
+    ; Function Attrs: argmemonly nofree nounwind willreturn
+    declare void @llvm.memcpy.p0i8.p0i8.i64(i8* noalias nocapture writeonly, i8* noalias nocapture readonly, i64, i1 immarg) #0
+
+    attributes #0 = { argmemonly nofree nounwind willreturn }
+    "###);
+}
+
+/// Accessing STRUCT's members uses the LLVM GEP statement to get a pointer
+/// to the struct's elements.
+#[test]
+fn accessing_struct_members() {
+    // two nested struct-variables that get initialized ...
+    let src = r#"
+        TYPE Point:
+        STRUCT
+            x, y   : INT;
+        END_STRUCT
+        END_TYPE
+
+        TYPE Rect:
+        STRUCT
+            topLeft, bottomRight   : Point;
+        END_STRUCT
+        END_TYPE
+
+        PROGRAM prg
+            VAR
+                rect1  : Rect;
+                rect2  : Rect;
+            END_VAR
+
+            rect1.topLeft.x := rect2.bottomRight.x;
+        END_PROGRAM
+        "#;
+
+    // ... will be initialized directly in the variable's definition
+    insta::assert_snapshot!(codegen(src), @r###"
+    ; ModuleID = 'main'
+    source_filename = "main"
+
+    %prg = type { %Rect, %Rect }
+    %Rect = type { %Point, %Point }
+    %Point = type { i16, i16 }
+
+    @prg_instance = global %prg zeroinitializer
+    @__Point__init = unnamed_addr constant %Point zeroinitializer
+    @__Rect__init = unnamed_addr constant %Rect zeroinitializer
+
+    define void @prg(%prg* %0) {
+    entry:
+      %rect1 = getelementptr inbounds %prg, %prg* %0, i32 0, i32 0
+      %rect2 = getelementptr inbounds %prg, %prg* %0, i32 0, i32 1
+      %topLeft = getelementptr inbounds %Rect, %Rect* %rect1, i32 0, i32 0
+      %x = getelementptr inbounds %Point, %Point* %topLeft, i32 0, i32 0
+      %bottomRight = getelementptr inbounds %Rect, %Rect* %rect2, i32 0, i32 1
+      %x1 = getelementptr inbounds %Point, %Point* %bottomRight, i32 0, i32 0
+      %load_ = load i16, i16* %x1, align 2
+      store i16 %load_, i16* %x, align 2
+      ret void
+    }
+    "###);
+}
