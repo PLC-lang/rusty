2073bf567cec6ec6a46a4a1ca712e44ba0828c79
Michael <78988079+mhasel@users.noreply.github.com>
2023-03-17 10:05:55 +0100

    llvm_typesystem refactor (#798)
    * reorganizes llvm typecasting. implements castable traits for inkwell basic values.
    
    * removes diagnostics from llvm_typesystem. all cast statements should be validated beforehand and  errors will now fail as unreachable instead
    
    * improves error messages. removes duplicate type-size checks when truncating or promoting
    
    * removes generic guard. code branch is never reached, generics are already resolved and validated before this stage
    
    * updates 'cast_if_needed' docstring
    
    * replaces codegen-casting tests with equivalent validation test

--- src/codegen/generators/variable_generator.rs
+++ src/codegen/generators/variable_generator.rs
@@ -3,14 +3,12 @@
 /// offers operations to generate global variables
 use crate::{
     ast::SourceRange,
-    codegen::{debug::Debug, llvm_typesystem::cast_if_needed},
+    codegen::{debug::Debug, llvm_index::LlvmTypedIndex, llvm_typesystem::cast_if_needed},
     diagnostics::{Diagnostic, ErrNo},
-    index::{Index, PouIndexEntry},
+    index::{Index, PouIndexEntry, VariableIndexEntry},
     resolver::{AnnotationMap, AstAnnotations},
 };
 use inkwell::{module::Module, values::GlobalValue};
-
-use crate::{codegen::llvm_index::LlvmTypedIndex, index::VariableIndexEntry};
 
 use super::{
     data_type_generator::get_default_for,
@@ -101,12 +99,7 @@ pub fn generate_global_variable<'ctx, 'b>(
             let value = expr_generator.generate_expression(initializer)?;
             let target_type = global_index.get_effective_type_or_void_by_name(type_name);
             let value_type = annotations.get_type_or_void(initializer, global_index);
-            Some(
-                match cast_if_needed(llvm, global_index, index, target_type, value, value_type, initializer) {
-                    Ok(v) => v,
-                    Err(_) => value,
-                },
-            )
+            Some(cast_if_needed(llvm, global_index, index, target_type, value_type, value))
         }
     } else {
         None
