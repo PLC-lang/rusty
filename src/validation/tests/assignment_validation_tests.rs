694e6a4f40a4f6c39ec3d867951c92d0991d1b21
Michael <78988079+mhasel@users.noreply.github.com>
2023-03-20 11:52:18 +0100

    729 generate warnings if assigning with implicit downcasts (#760)
    * adds validation for implicit function call parameter truncation
    
    * changes downcast diagnostic to use new push_diagnostic method
    
    * adds implicit downcast validation for actions and methods
    
    adds exceptions for aggregate types (these are skipped: mismatches are reported as errors elsewhere)
    
    adds exception for generic POUs
    
    adds exception for passing literals
    
    adds proper by_ref and pointer resolution, so LWORD being assigned to REF_TO parameters is no longer
    falsely reported.
    
    adds tests for actions and methods
    
    * added test to show downcast detection with the help of annotations
    
    * change find_pou method to use get_call_name method from ast
    
    * adds downcast warnings for all assignments
    
    * adds resolver tests
    
    * removes char conversion codegen test
    
    ---------
    
    Co-authored-by: Ghaith Hachem <ghaith.hachem@gmail.com>
    Co-authored-by: Mathias Rieder <mathias.rieder@gmail.com>
    Co-authored-by: Volkan <volkan.sagcan@bachmann.info>

--- src/validation/tests/assignment_validation_tests.rs
+++ src/validation/tests/assignment_validation_tests.rs
@@ -798,3 +798,64 @@ fn invalid_method_call_assignments_are_validated() {
 
     assert_validation_snapshot!(&diagnostics)
 }
+
+#[test]
+fn invalid_function_block_instantiation_is_validated() {
+    let diagnostics = parse_and_validate(
+        r#"
+        FUNCTION_BLOCK fb_t
+        VAR_INPUT
+            ws : WSTRING;
+        arr_32 : ARRAY[0..1] OF DINT;
+        END_VAR
+        END_FUNCTION_BLOCK
+    
+        PROGRAM prog
+        VAR
+            s : STRING := 'HELLO';
+            arr_64 : ARRAY[0..1] OF LINT;
+            fb : fb_t;
+        END_VAR
+            fb(ws := s, arr_32 := arr_64); // invalid explicit
+            fb(s, arr_64); // invalid implicit
+        END_PROGRAM"#,
+    );
+
+    assert_validation_snapshot!(&diagnostics);
+}
+
+#[test]
+fn implicit_action_downcasts_are_validated() {
+    let diagnostics = parse_and_validate(
+        r#"
+        FUNCTION_BLOCK fb_t
+        VAR
+            var1 : ARRAY[0..10] OF WSTRING;
+            var2 : ARRAY[0..10] OF WSTRING;
+        END_VAR       
+        VAR_INPUT
+            in1 : DINT;
+            in2 : DWORD;
+            in3 : BYTE;
+        END_VAR 
+        END_FUNCTION_BLOCK
+        
+        ACTIONS fb_t
+        ACTION foo
+        END_ACTION
+        END_ACTIONS
+
+        FUNCTION main : DINT
+        VAR
+            fb: fb_t;
+            var1 : LINT;
+            var2 : LWORD;
+            var3 : STRING;
+        END_VAR
+            fb.foo(var1, var2, var3);
+        END_FUNCTION
+        "#,
+    );
+
+    assert_validation_snapshot!(&diagnostics);
+}
