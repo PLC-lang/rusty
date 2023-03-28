dbca4daef2eb68ee83580d9a30d1c26a65654742
Volkan Sagcan <volkanxvs@gmail.com>
2023-03-28 16:04:09 +0200

    Add integration test for single dimension access

--- tests/correctness/vla.rs
+++ tests/correctness/vla.rs
@@ -1,34 +1,49 @@
 use rusty::runner::compile_and_run;
 
 #[test]
-fn vla() {
+fn variable_length_array_single_dimension_access() {
     #[derive(Default)]
     struct MainType {
-        i: i32,
+        a: i32,
+        b: i32,
+        c: i32,
+        d: i32,
+        e: i32,
     }
+
     let mut main_type = MainType::default();
-
     let src = r#"
-    FUNCTION main : DINT
-    VAR
-        i : DINT;
-    END_VAR
-    VAR_TEMP
-        arr : ARRAY[0..1] OF DINT := (5, 6);
-    END_VAR
+    PROGRAM main
+        VAR
+            a, b, c, d, e : DINT;
+            arr : ARRAY[0..4] OF DINT;
+        END_VAR
 
-        main := foo(arr);
+        foo(arr);
+        a := arr[0];
+        b := arr[1];
+        c := arr[2];
+        d := arr[3];
+        e := arr[4];
     END_PROGRAM
 
     FUNCTION foo : DINT
-    VAR_INPUT
-        vla : ARRAY[ * ] OF DINT;
-    END_VAR       
+        VAR_INPUT
+            vla : ARRAY[ * ] OF DINT;
+        END_VAR
 
-        foo := vla[0];
+        vla[0] := 2;
+        vla[1] := 4;
+        vla[2] := 6;
+        vla[3] := 8;
+        vla[4] := 10;
     END_FUNCTION
     "#;
 
     let _: i32 = compile_and_run(src.to_string(), &mut main_type);
-    assert_eq!(6, main_type.i)
+    assert_eq!(2, main_type.a);
+    assert_eq!(4, main_type.b);
+    assert_eq!(6, main_type.c);
+    assert_eq!(8, main_type.d);
+    assert_eq!(10, main_type.e);
 }
