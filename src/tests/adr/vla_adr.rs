9fa4e4dcdfd9c1eb2afbe917bc1cde8805efcfab
Volkan Sagcan <volkanxvs@gmail.com>
2023-04-17 18:03:50 +0200

    Add ADR draft

--- src/tests/adr/vla_adr.rs
+++ src/tests/adr/vla_adr.rs
@@ -1,33 +1,34 @@
+//! ST supports the concept of Variable Length Arrays (VLA), allowing users to pass array arguments of
+//! different sizes to a function, function-block or method POU. These VLAs can be declared using an asterisk
+//! instead of a range statement, such that `arr : ARRAY[*] OF DINT` is a valid declaration. In the given example
+//! we pass two arrays of different sizes (`local_a`, `local_b`) to the `foo` function accepting any 1D array
+//! of type DINT. Any read and write access is thereby valid, as long as we're not out-of-bounds. [...]
+
 use crate::{
     lexer::IdProvider,
+    resolver::AnnotationMap,
     test_utils::tests::{annotate_with_ids, codegen, index_with_ids},
+    tests::adr::util_macros::{annotate, deconstruct_call_statement},
 };
 
-/// TODO: explain VLAs in general, i.e. what they do and/or what they're used for
-static SOURCE: &str = "
-    FUNCTION foo : DINT
-        VAR
-            arr : ARRAY[*] OF DINT;
-        END_VAR
-    END_FUNCTION
-
-    FUNCTION main : DINT
-        VAR
-            local : ARRAY[0..1] OF DINT;
-        END_VAR
-
-        foo(local);
-    END_FUNCTION
-";
-
-/// TODO: Explain how VLAs are internally represented, i.e. expanded into fat-pointer, fields of fat-pointer
+/// While declared as an array, VLAs are internally expanded into so called fat-pointer structs. These structs
+/// carry two metadata fields, namely (1) a pointer to an array and (2) an array with information about the
+/// pointed arrays dimensions and sizes. These fields and how they're populated will be explained in the
+/// [`pass`] test, here we just want to showcase how these VLAs are represented internally in the index.
 #[test]
 fn ddeclare() {
-    let id_provider = IdProvider::default();
-    let (pr, mut index) = index_with_ids(SOURCE, id_provider.clone());
-    // process pou and types
-    annotate_with_ids(&pr, &mut index, id_provider);
+    let (_, _, index) = annotate!(
+        r"
+        FUNCTION foo : DINT
+            VAR_INPUT {ref}
+                arr : ARRAY[*] OF DINT;
+            END_VAR
+        END_FUNCTION
+        "
+    );
 
+    // The probably most interesting entry here is the `source` field, indicating that the given struct is a
+    // VLA with one dimension of type DINT.
     insta::assert_debug_snapshot!(index.find_effective_type_by_name("__foo_arr").unwrap(), 
     @r###"
         DataType {
@@ -88,37 +89,298 @@ fn ddeclare() {
             location: SymbolLocation {
                 line_number: 3,
                 source_range: SourceRange {
-                    range: 67..83,
+                    range: 79..95,
+                },
+            },
+        }
+    "###);
+
+    insta::assert_debug_snapshot!(index.find_effective_type_by_name("ptr_to___arr_vla_1_dint").unwrap(), 
+    @r###"
+        DataType {
+            name: "ptr_to___arr_vla_1_dint",
+            initial_value: None,
+            information: Pointer {
+                name: "ptr_to___arr_vla_1_dint",
+                inner_type_name: "__arr_vla_1_dint",
+                auto_deref: false,
+            },
+            nature: Any,
+            location: SymbolLocation {
+                line_number: 0,
+                source_range: SourceRange {
+                    range: 0..0,
+                },
+            },
+        }
+    "###);
+
+    // TODO: Why only one dimension
+    insta::assert_debug_snapshot!(index.find_effective_type_by_name("n_dims").unwrap(), 
+    @r###"
+        DataType {
+            name: "n_dims",
+            initial_value: None,
+            information: Array {
+                name: "n_dims",
+                inner_type_name: "DINT",
+                dimensions: [
+                    Dimension {
+                        start_offset: ConstExpression(
+                            Index {
+                                index: 0,
+                                generation: 0,
+                            },
+                        ),
+                        end_offset: ConstExpression(
+                            Index {
+                                index: 1,
+                                generation: 0,
+                            },
+                        ),
+                    },
+                ],
+            },
+            nature: Any,
+            location: SymbolLocation {
+                line_number: 0,
+                source_range: SourceRange {
+                    range: 0..0,
                 },
             },
         }
     "###);
 }
 
-/// TODO: Explain how VLAs are translated from internal representation into LLVM IR
+/// Because VLAs are internally handled as structs, they'll naturally also translate into LLVM structs.
 #[test]
 fn declare() {
     let src = "
         FUNCTION foo : DINT
-            VAR
+            VAR_INPUT {ref}
                 arr : ARRAY[*] OF DINT;
             END_VAR
         END_FUNCTION
     ";
 
-    insta::assert_snapshot!(codegen(src), @r###"
-        ...
+    assert!(codegen(src).contains("%__foo_arr = type { i32*, [2 x i32] }"));
+}
+
+/// TODO: This is most definitely a bug, i.e. allocating should only happen once
+/// VLAs (in RuSTy) are defined to be always by-ref, meaning POUs accepting VLAs expect a pointer to a struct.
+/// To satisfy that constraint, any array passed to such a POU needs to be first wrapped inside a fat-pointer
+/// struct with their fields populated. Therefore, whenever we encounter such situations we first stack
+/// allocate a fat-pointer struct then pass (1) the arrays address and (2) an array consisiting of the
+/// dimensions' {start,end}-offset.
+#[test]
+fn pass() {
+    let src = "
+        FUNCTION main : DINT
+            VAR
+                local : ARRAY[0..5] OF DINT;
+            END_VAR
+
+            foo(local);
+        END_FUNCTION
+
+        FUNCTION foo : DINT
+            VAR_INPUT {ref}
+                arr : ARRAY[*] OF DINT;
+            END_VAR
+        END_FUNCTION
+    ";
+
+    let (statements, annotations, index) = annotate!(src);
+    let (_, local) = deconstruct_call_statement!(&statements[0]);
+
+    // `local` is defined as a ARRAY of type DINT...
+    insta::assert_debug_snapshot!(annotations.get_type(&local[0], &index).unwrap(), 
+    @r###"
+    DataType {
+        name: "__main_local",
+        initial_value: None,
+        information: Array {
+            name: "__main_local",
+            inner_type_name: "DINT",
+            dimensions: [
+                Dimension {
+                    start_offset: ConstExpression(
+                        Index {
+                            index: 0,
+                            generation: 0,
+                        },
+                    ),
+                    end_offset: ConstExpression(
+                        Index {
+                            index: 1,
+                            generation: 0,
+                        },
+                    ),
+                },
+            ],
+        },
+        nature: Any,
+        location: SymbolLocation {
+            line_number: 3,
+            source_range: SourceRange {
+                range: 70..89,
+            },
+        },
+    }
+    "###);
+
+    // ...but their type-hint indicates it should be fat-pointer struct. Thanks to these type-hints finding
+    // cases where arrays need to be wrapped before-hand is made easy.
+    let hint = annotations.get_type_hint(&local[0], &index).unwrap();
+    insta::assert_debug_snapshot!(index.find_elementary_pointer_type(&hint.information), 
+    @r###"
+    Struct {
+        name: "__foo_arr",
+        members: [
+            VariableIndexEntry {
+                name: "struct_vla_dint_1",
+                qualified_name: "__foo_arr.struct_vla_dint_1",
+                initial_value: None,
+                variable_type: ByVal(
+                    Input,
+                ),
+                is_constant: false,
+                data_type_name: "ptr_to___arr_vla_1_dint",
+                location_in_parent: 0,
+                linkage: Internal,
+                binding: None,
+                source_location: SymbolLocation {
+                    line_number: 0,
+                    source_range: SourceRange {
+                        range: 0..0,
+                    },
+                },
+                varargs: None,
+            },
+            VariableIndexEntry {
+                name: "dimensions",
+                qualified_name: "__foo_arr.dimensions",
+                initial_value: None,
+                variable_type: ByVal(
+                    Input,
+                ),
+                is_constant: false,
+                data_type_name: "n_dims",
+                location_in_parent: 1,
+                linkage: Internal,
+                binding: None,
+                source_location: SymbolLocation {
+                    line_number: 0,
+                    source_range: SourceRange {
+                        range: 0..0,
+                    },
+                },
+                varargs: None,
+            },
+        ],
+        source: Internal(
+            VariableLengthArray {
+                inner_type_name: "DINT",
+                ndims: 1,
+            },
+        ),
+    }
+    "###);
+
+    // Finally here's the codegen for populating fat-pointer struct, where we
+    // 1. Stack allocate a fat-pointer struct
+    // 2. GEP the structs array and dimension field
+    // 3. And populate them based on the information we have on `local`, i.e. 1D and (start, end)= (0, 5)
+    insta::assert_snapshot!(codegen(src), 
+    @r###"
+    ; ModuleID = 'main'
+    source_filename = "main"
+    
+    %__foo_arr = type { i32*, [2 x i32] }
+    
+    @____foo_arr__init = unnamed_addr constant %__foo_arr zeroinitializer
+    
+    define i32 @main() {
+    entry:
+      %main = alloca i32, align 4
+      %local = alloca [6 x i32], align 4
+      %0 = bitcast [6 x i32]* %local to i8*
+      call void @llvm.memset.p0i8.i64(i8* align 1 %0, i8 0, i64 ptrtoint ([6 x i32]* getelementptr ([6 x i32], [6 x i32]* null, i32 1) to i64), i1 false)
+      store i32 0, i32* %main, align 4
+      %auto_deref = load [6 x i32], [6 x i32]* %local, align 4
+      %1 = bitcast [6 x i32]* %local to i32*
+      %vla_struct = alloca %__foo_arr, align 8
+      %vla_array_gep = getelementptr inbounds %__foo_arr, %__foo_arr* %vla_struct, i32 0, i32 0
+      %vla_dimensions_gep = getelementptr inbounds %__foo_arr, %__foo_arr* %vla_struct, i32 0, i32 1
+      %2 = getelementptr inbounds [2 x i32], [2 x i32]* %vla_dimensions_gep, i32 0, i32 0
+      store i32 0, i32* %2, align 4
+      %3 = getelementptr inbounds [2 x i32], [2 x i32]* %vla_dimensions_gep, i32 0, i32 1
+      store i32 5, i32* %3, align 4
+      store i32* %1, i32** %vla_array_gep, align 8
+      %4 = load %__foo_arr, %__foo_arr* %vla_struct, align 8
+      %vla_struct_ptr = alloca %__foo_arr, align 8
+      store %__foo_arr %4, %__foo_arr* %vla_struct_ptr, align 8
+      %call = call i32 @foo(%__foo_arr* %vla_struct_ptr)
+      %main_ret = load i32, i32* %main, align 4
+      ret i32 %main_ret
+    }
+    
+    define i32 @foo(%__foo_arr* %0) {
+    entry:
+      %foo = alloca i32, align 4
+      %arr = alloca %__foo_arr*, align 8
+      store %__foo_arr* %0, %__foo_arr** %arr, align 8
+      store i32 0, i32* %foo, align 4
+      %foo_ret = load i32, i32* %foo, align 4
+      ret i32 %foo_ret
+    }
+    
+    ; Function Attrs: argmemonly nofree nounwind willreturn writeonly
+    declare void @llvm.memset.p0i8.i64(i8* nocapture writeonly, i8, i64, i1 immarg) #0
+
+    attributes #0 = { argmemonly nofree nounwind willreturn writeonly }
     "###);
 }
 
-/// TODO: Explain how local arrays are passed to POUs expecting VLAs, i.e. transformation from array -> vla
 #[test]
-fn pass() {}
+fn access() {
+    let src = "
+        FUNCTION foo : DINT
+            VAR_INPUT {ref}
+                arr : ARRAY[*] OF DINT;
+            END_VAR
 
-/// TODO: explain read-access
-#[test]
-fn access_read() {}
+            arr[0] := 12345;
+        END_FUNCTION
+    ";
 
-/// TODO: explain write-access
-#[test]
-fn access_write() {}
+    insta::assert_snapshot!(codegen(src), 
+    @r###"
+    ; ModuleID = 'main'
+    source_filename = "main"
+
+    %__foo_arr = type { i32*, [2 x i32] }
+
+    @____foo_arr__init = unnamed_addr constant %__foo_arr zeroinitializer
+
+    define i32 @foo(%__foo_arr* %0) {
+    entry:
+      %foo = alloca i32, align 4
+      %arr = alloca %__foo_arr*, align 8
+      store %__foo_arr* %0, %__foo_arr** %arr, align 8
+      store i32 0, i32* %foo, align 4
+      %auto_deref = load %__foo_arr*, %__foo_arr** %arr, align 8
+      %vla_arr_gep = getelementptr inbounds %__foo_arr, %__foo_arr* %auto_deref, i32 0, i32 0
+      %vla_arr_ptr = load i32*, i32** %vla_arr_gep, align 8
+      %dim_arr = getelementptr inbounds %__foo_arr, %__foo_arr* %auto_deref, i32 0, i32 1
+      %start_idx_ptr = getelementptr inbounds [2 x i32], [2 x i32]* %dim_arr, i32 0, i32 0
+      %end_idx_ptr = getelementptr inbounds [2 x i32], [2 x i32]* %dim_arr, i32 0, i32 1
+      %start_offset = load i32, i32* %start_idx_ptr, align 4
+      %tmpVar = sub i32 0, %start_offset
+      %arr_val = getelementptr inbounds i32, i32* %vla_arr_ptr, i32 %tmpVar
+      store i32 12345, i32* %arr_val, align 4
+      %foo_ret = load i32, i32* %foo, align 4
+      ret i32 %foo_ret
+    }
+    "###);
+}
