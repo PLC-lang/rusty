f9b14113b88b46fd58e376fcb1a60ce55746b132
Volkan Sagcan <volkanxvs@gmail.com>
2023-04-17 10:16:29 +0200

    Update oscat task reflecting workspaces

--- xtask/src/metrics/oscat.rs
+++ xtask/src/metrics/oscat.rs
@@ -5,22 +5,17 @@ pub struct Oscat;
 impl Task for Oscat {
     fn prepare(&self, sh: &Shell) -> anyhow::Result<()> {
         cmd!(sh, "git clone https://github.com/plc-lang/oscat ./benchmark/oscat").run()?;
-        cmd!(sh, "git clone https://github.com/plc-lang/standardfunctions ./benchmark/oscat/sf").run()?;
-
-        cmd!(sh, "cargo b --release").run()?;
-        sh.copy_file("./target/release/rustyc", "./benchmark/oscat")?;
-
         sh.create_dir("./benchmark/oscat/lib").unwrap();
         sh.create_dir("./benchmark/oscat/include").unwrap();
 
-        let standardfunctions = sh.push_dir("./benchmark/oscat/sf");
         cmd!(sh, "cargo b --release").run()?;
-        std::mem::drop(standardfunctions);
+        sh.copy_file("./target/release/rustyc", "./benchmark/oscat")?;
+        sh.copy_file("./target/release/libiec61131std.so", "./benchmark/oscat/lib").unwrap();
 
-        let oscat = sh.push_dir("./benchmark/oscat");
-        sh.copy_file("sf/target/release/libiec61131std.so", "lib").unwrap();
-        sh.read_dir("sf/iec61131-st/").unwrap().iter().for_each(|f| sh.copy_file(f, "include").unwrap());
-        std::mem::drop(oscat);
+        sh.read_dir("libs/stdlib/iec61131-st")
+            .unwrap()
+            .iter()
+            .for_each(|f| sh.copy_file(f, "./benchmark/oscat/include").unwrap());
 
         Ok(())
     }
