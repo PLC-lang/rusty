709088344cf406f3f22bd1496f796c2dc4836288
Volkan Sagcan <volkanxvs@gmail.com>
2023-04-18 16:40:34 +0200

    Working prototype

--- src/validation/variable.rs
+++ src/validation/variable.rs
@@ -1,6 +1,8 @@
 use crate::{
-    ast::{AstStatement, Variable, VariableBlock, VariableBlockType},
+    ast::{AstStatement, DataTypeDeclaration, Variable, VariableBlock, VariableBlockType},
     index::const_expressions::ConstExpression,
+    resolver::const_evaluator,
+    typesystem::DataTypeInformation,
     Diagnostic,
 };
 
@@ -27,10 +29,51 @@ fn validate_variable_block(validator: &mut Validator, block: &VariableBlock) {
 
 pub fn visit_variable(validator: &mut Validator, variable: &Variable, context: &ValidationContext) {
     validate_variable(validator, variable, context);
+    temp(validator, variable, context);
 
     visit_data_type_declaration(validator, &variable.data_type_declaration, context);
 }
 
+fn temp(validator: &mut Validator, variable: &Variable, context: &ValidationContext) {
+    let Some(initializer) = &variable.initializer else { return };
+    let Ok(initializer) = const_evaluator::evaluate(&initializer, None, context.index) else { return };
+
+    let DataTypeDeclaration::DataTypeReference { referenced_type, .. } = &variable.data_type_declaration else {
+        return
+    };
+
+    let Some(dt) = context.index.find_effective_type_by_name(&referenced_type) else { return };
+    let err = match dt.get_type_information() {
+        DataTypeInformation::Integer { signed, size, .. } => match (size, signed, initializer) {
+            (8, true, Some(AstStatement::LiteralInteger { value, .. })) => i8::try_from(value).is_err(),
+            (16, true, Some(AstStatement::LiteralInteger { value, .. })) => i16::try_from(value).is_err(),
+            (32, true, Some(AstStatement::LiteralInteger { value, .. })) => i32::try_from(value).is_err(),
+            (64, true, Some(AstStatement::LiteralInteger { value, .. })) => i64::try_from(value).is_err(),
+
+            (8, false, Some(AstStatement::LiteralInteger { value, .. })) => i8::try_from(value).is_err(),
+            (16, false, Some(AstStatement::LiteralInteger { value, .. })) => i16::try_from(value).is_err(),
+            (32, false, Some(AstStatement::LiteralInteger { value, .. })) => i32::try_from(value).is_err(),
+            (64, false, Some(AstStatement::LiteralInteger { value, .. })) => i64::try_from(value).is_err(),
+
+            _ => return,
+        },
+
+        // DataTypeInformation::Float { size, .. } => match (size, initializer) {
+        //     (32, Some(AstStatement::LiteralReal { value, .. })) => value.parse::<f32>().is_err(),
+        //     (64, Some(AstStatement::LiteralReal { value, .. })) => value.parse::<f64>().is_err(),
+        // },
+        _ => return,
+    };
+    if err {
+        validator.push_diagnostic(Diagnostic::SemanticError {
+            message: format!("Literal out of range for {}", dt.get_name()),
+            range: vec![variable.initializer.as_ref().unwrap().get_location()],
+            err_no: crate::diagnostics::ErrNo::type__incompatible_literal_cast,
+        });
+    }
+}
+
+// TODO: document what the fuck this does
 fn validate_variable(validator: &mut Validator, variable: &Variable, context: &ValidationContext) {
     if let Some(v_entry) = context
         .qualifier
