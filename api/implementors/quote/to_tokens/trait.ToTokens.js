eab838e39b6ec83cc53e74628db6073d425c2b10
mhasel <mhasel@users.noreply.github.com>
2023-05-10 07:30:17 +0000

    deploy: fe8b2ccaf27a828874f31aa626bd2fb5386d635e

--- api/implementors/quote/to_tokens/trait.ToTokens.js
+++ api/implementors/quote/to_tokens/trait.ToTokens.js
@@ -1,4 +1,3 @@
 (function() {var implementors = {
-"proc_macro_error":[["impl <a class=\"trait\" href=\"quote/to_tokens/trait.ToTokens.html\" title=\"trait quote::to_tokens::ToTokens\">ToTokens</a> for <a class=\"struct\" href=\"proc_macro_error/struct.Diagnostic.html\" title=\"struct proc_macro_error::Diagnostic\">Diagnostic</a>"]],
-"quote":[]
+"proc_macro_error":[["impl <a class=\"trait\" href=\"https://docs.rs/quote/1.0.26/quote/to_tokens/trait.ToTokens.html\" title=\"trait quote::to_tokens::ToTokens\">ToTokens</a> for <a class=\"struct\" href=\"proc_macro_error/struct.Diagnostic.html\" title=\"struct proc_macro_error::Diagnostic\">Diagnostic</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
