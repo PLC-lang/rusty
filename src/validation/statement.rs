1d22907abb082f7fefd0a892a440517a2eb3e21c
Volkan Sagcan <volkanxvs@gmail.com>
2023-05-11 16:31:31 +0200

    Clippy :(

--- src/validation/statement.rs
+++ src/validation/statement.rs
@@ -630,10 +630,9 @@ fn is_valid_assignment(
         // in this case return true and skip any other validation
         // because those would fail
         return true;
-    }
+    } else if is_invalid_char_assignment(left_type.get_type_information(), right_type.get_type_information())
     // FIXME: See https://github.com/PLC-lang/rusty/issues/857
     // else if is_invalid_pointer_assignment(left_type.get_type_information(), right_type.get_type_information(), index, location, validator) |
-    else if is_invalid_char_assignment(left_type.get_type_information(), right_type.get_type_information())
         | is_aggregate_to_none_aggregate_assignment(left_type, right_type)
         | is_aggregate_type_missmatch(left_type, right_type, index)
     {
