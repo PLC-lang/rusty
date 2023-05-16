85d44e1c36f79fa0f9f6157cfcff7920b652ae10
Volkan Sagcan <volkanxvs@gmail.com>
2023-05-16 10:11:35 +0200

    Snapshots

--- src/validation/tests/variable_length_array_test.rs
+++ src/validation/tests/variable_length_array_test.rs
@@ -176,46 +176,6 @@ mod assignment {
 
         assert_validation_snapshot!(diagnostics);
     }
-
-    #[test]
-    fn input_by_ref_should_deliver_improvment_suggestion() {
-        let diagnostics = parse_and_validate(
-            "
-            FUNCTION fn : DINT
-                VAR_INPUT
-                    a : DINT;
-                END_VAR
-
-                VAR_INPUT {ref}
-                    b : REAL;
-                    c : REAL;
-                END_VAR
-
-                VAR_IN_OUT
-                    d : LREAL;
-                END_VAR
-
-                a := 1;
-                b := 1.0;   // This should trigger an improvment suggestion, because we are assigning a value
-                c;          // This should NOT trigger an improvment suggestion, because we are NOT assigning a value
-                d := 1.0;
-            END_FUNCTION
-
-            FUNCTION main : DINT
-                VAR
-                    a : DINT = 3;
-                    b : REAL := 3.14;
-                    c : REAL := 3.14;
-                    d : LREAL := 3.14;
-                END_VAR
-
-                fn(a, b, c, d);
-            END_FUNCTION
-            ",
-        );
-
-        assert_validation_snapshot!(diagnostics);
-    }
 }
 
 mod builtins {
