6fb530c667c58ab36e19e7ff090b3cb8c5ba2b3b
Michael HASELBERGER <Michael.HASELBERGER@bachmann.info>
2023-06-16 13:00:32 +0200

    fix error in test-file. cfc-pipeline now works :)

--- tests/integration/cfc.rs
+++ tests/integration/cfc.rs
@@ -7,12 +7,6 @@ fn cfc_variables_pass_through() {
     let cfc_file = get_test_file("cfc/pass_through.xml");
     let st_file = get_test_file("cfc/call_pass_through.st");
 
-    struct MainType {
-        x: i32,
-        y: i32,
-    }
-
-    let mut maintype = MainType { x: 100, y: 200 };
-    let res: i32 = compile_and_run(vec![st_file, cfc_file], &mut maintype);
-    assert_eq!(maintype.x + maintype.y, res);
+    let res: i32 = compile_and_run(vec![st_file, cfc_file], &mut {});
+    assert_eq!(res, 300);
 }
