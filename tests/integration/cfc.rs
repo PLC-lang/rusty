3077a379dca0f90aa3229d77e10808e5d1980fd2
Michael HASELBERGER <Michael.HASELBERGER@bachmann.info>
2023-06-23 13:43:40 +0200

    add integration test for function returns

--- tests/integration/cfc.rs
+++ tests/integration/cfc.rs
@@ -3,10 +3,10 @@ use driver::runner::compile_and_run;
 use crate::get_test_file;
 
 #[test]
-fn variables_pass_through() {
+fn variables_assigned() {
     // GIVEN a ST program with hardcoded values and CFC program with two variables but without a body
-    let st_file = get_test_file("cfc/pass_through.st");
-    let cfc_file = get_test_file("cfc/pass_through.cfc");
+    let st_file = get_test_file("cfc/assigning.st");
+    let cfc_file = get_test_file("cfc/assigning.cfc");
 
     // WHEN assigning these values to the CFC-POU variables and adding them together
     let res: i32 = compile_and_run(vec![st_file, cfc_file], &mut {});
@@ -68,4 +68,15 @@ fn chained_calls_galore() {
     let res: i32 = compile_and_run(vec![st_file, cfc_file], &mut {});
     // THEN the second variable will have the value of the first variable
     assert_eq!(res, 88);
+}
+
+#[test]
+fn function_returns() {
+    // GIVEN a CFC function which doubles a value
+    let st_file = get_test_file("cfc/function_returns.st");
+    let cfc_file = get_test_file("cfc/function_returns.cfc");
+    // WHEN passing a value into the function
+    let res: i32 = compile_and_run(vec![st_file, cfc_file], &mut {});
+    // THEN it will return the correct value
+    assert_eq!(res, 222);
 }
