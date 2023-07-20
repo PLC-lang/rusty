d958e40e734e182afbf359609a1483028d329013
Volkan <volkanxvs@gmail.com>
2023-07-20 11:37:58 +0200

    Lexer Metrics (#903)
    Introduces benchmarking the Lexer with the corpus being all ST files in our repository (with some post-process cleanup)

--- xtask/src/task.rs
+++ xtask/src/task.rs
@@ -2,27 +2,35 @@ use anyhow::Result;
 use std::{path::Path, time::Duration};
 use xshell::{cmd, Shell};
 
+use crate::{reporter::DurationFormat, task::lexer::Lexer};
+
 use self::{compile::Compile, run::Run};
 
 pub(crate) mod compile;
+pub(crate) mod lexer;
 pub(crate) mod run;
 
 pub(crate) trait Task {
     /// Returns the name of the task being benchmarked
     fn get_name(&self) -> String;
+
+    /// Returns the [`DurationFormat`] the task was benchmarked in
+    fn get_duration_format(&self) -> DurationFormat;
+
     /// Executes any actions required before time measurement starts
     /// By default we make sure we have a release build ready
     fn prepare(&mut self) -> Result<()> {
         Ok(())
     }
+
     /// Executes the task to be measured and returns the time it took
     fn execute(&self) -> Result<Duration>;
 
     /// Benchmarks the current task and returns the avarage execution time
     fn benchmark(&mut self, executions: u32) -> Result<Duration> {
         self.prepare()?;
-        //Cold run
-        self.execute()?;
+        self.execute()?; // Cold Run
+
         let mut duration = Duration::from_millis(0);
         for _ in 0..executions {
             duration += self.execute()?;
@@ -33,13 +41,21 @@ pub(crate) trait Task {
 }
 
 pub(crate) fn get_default_tasks(work_dir: &Path, compiler: &Path) -> Result<Vec<Box<dyn Task>>> {
-    //Clone the extra required code
-    println!("Clone Oscat into the benchmarks");
     let sh = Shell::new()?;
     cmd!(&sh, "git clone https://github.com/plc-lang/oscat --depth 1 {work_dir}/oscat").run()?;
+
     let mut tasks: Vec<Box<dyn Task>> = vec![];
-    //Create a default benchmark run
-    //This includes oscat in 4 different opt
+    tasks.extend(oscat(work_dir, compiler));
+    tasks.extend(sieve_st(work_dir, compiler));
+    tasks.extend(sieve_c(work_dir));
+    tasks.push(Box::new(Lexer("combined.st")));
+
+    Ok(tasks)
+}
+
+/// Benchmark task for `oscat`
+fn oscat(work_dir: &Path, compiler: &Path) -> Vec<Box<dyn Task>> {
+    let mut tasks: Vec<Box<dyn Task>> = vec![];
     for opt in &["none", "less", "default", "aggressive"] {
         let task = Compile {
             name: "oscat".into(),
@@ -47,11 +63,16 @@ pub(crate) fn get_default_tasks(work_dir: &Path, compiler: &Path) -> Result<Vec<
             directory: work_dir.join("oscat"),
             optimization: opt.to_string(),
         };
+
         tasks.push(Box::new(task));
     }
 
-    // This includes the sieve of eratosthenes in
-    // C
+    tasks
+}
+
+/// Benchmark task for `res/sieve.c`
+fn sieve_c(work_dir: &Path) -> Vec<Box<dyn Task>> {
+    let mut tasks: Vec<Box<dyn Task>> = vec![];
     for opt in ["0", "1", "2", "3"] {
         let task = Run {
             name: "sieve-c".into(),
@@ -61,9 +82,16 @@ pub(crate) fn get_default_tasks(work_dir: &Path, compiler: &Path) -> Result<Vec<
             parameters: None,
             work_dir: work_dir.into(),
         };
+
         tasks.push(Box::new(task));
     }
-    // and ST
+
+    tasks
+}
+
+/// Benchmark task for `res/sieve.st`
+fn sieve_st(work_dir: &Path, compiler: &Path) -> Vec<Box<dyn Task>> {
+    let mut tasks: Vec<Box<dyn Task>> = vec![];
     for opt in ["none", "less", "default", "aggressive"] {
         let task = Run {
             name: "sieve-st".into(),
@@ -73,7 +101,9 @@ pub(crate) fn get_default_tasks(work_dir: &Path, compiler: &Path) -> Result<Vec<
             parameters: Some("--linker=cc".into()),
             work_dir: work_dir.into(),
         };
+
         tasks.push(Box::new(task));
     }
-    Ok(tasks)
+
+    tasks
 }
