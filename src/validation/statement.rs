0b23cd1c690900b99571de2b57c7e1af82e07105
Michael <78988079+mhasel@users.noreply.github.com>
2023-07-23 13:04:07 +0200

    fix: detect invalid enum variants in var blocks. (#904)
    reclassified warning as error

--- src/validation/statement.rs
+++ src/validation/statement.rs
@@ -576,21 +576,13 @@ fn validate_assignment<T: AnnotationMap>(
                 ));
             } else {
                 // ...enum variable where the RHS does not match its variants
-                let left_dt =
-                    context.annotations.get_type_or_void(left, context.index).get_type_information();
-                if left_dt.is_enum()
-                    && left_dt.get_name()
-                        != context
-                            .annotations
-                            .get_type_or_void(right, context.index)
-                            .get_type_information()
-                            .get_name()
-                {
-                    validator.push_diagnostic(Diagnostic::enum_variant_mismatch(
-                        qualified_name,
-                        right.get_location(),
-                    ))
-                }
+                validate_enum_variant_assignment(
+                    validator,
+                    context.annotations.get_type_or_void(left, context.index).get_type_information(),
+                    context.annotations.get_type_or_void(right, context.index).get_type_information(),
+                    qualified_name,
+                    right.get_location(),
+                );
             }
 
             // ...VAR_INPUT {ref} variable
@@ -639,6 +631,18 @@ fn validate_assignment<T: AnnotationMap>(
             // FIXME: See https://github.com/PLC-lang/rusty/issues/857
             // validate_assignment_type_sizes(validator, left_type, right_type, location, context)
         }
+    }
+}
+
+pub(crate) fn validate_enum_variant_assignment(
+    validator: &mut Validator,
+    left: &DataTypeInformation,
+    right: &DataTypeInformation,
+    qualified_name: &str,
+    location: SourceRange,
+) {
+    if left.is_enum() && left.get_name() != right.get_name() {
+        validator.push_diagnostic(Diagnostic::enum_variant_mismatch(qualified_name, location))
     }
 }
 
