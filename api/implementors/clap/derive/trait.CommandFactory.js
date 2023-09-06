1e860dc2a4595f37b379cb95efcef81a495e5a9d
volsa <volsa@users.noreply.github.com>
2023-09-06 13:00:35 +0000

    deploy: 31572d264b9c0b3ec7014875ea3235571b9b4635

--- api/implementors/clap/derive/trait.CommandFactory.js
+++ api/implementors/clap/derive/trait.CommandFactory.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"plc_driver":[["impl CommandFactory for <a class=\"struct\" href=\"plc_driver/cli/struct.CompileParameters.html\" title=\"struct plc_driver::cli::CompileParameters\">CompileParameters</a>"]]
+"plc_driver":[["impl <a class=\"trait\" href=\"clap/derive/trait.CommandFactory.html\" title=\"trait clap::derive::CommandFactory\">CommandFactory</a> for <a class=\"struct\" href=\"plc_driver/cli/struct.CompileParameters.html\" title=\"struct plc_driver::cli::CompileParameters\">CompileParameters</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
