4829c1522c4b6fa42c56e4f54df3ccf36ec98eb5
volsa <volsa@users.noreply.github.com>
2023-09-12 08:25:32 +0000

    deploy: 3690b1aff9a950981e3b49e35ffd692931123af6

--- api/plc_ast/control_statements/sidebar-items.js
+++ api/plc_ast/control_statements/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["AstControlStatement"],"struct":["CaseStatement","ConditionalBlock","ForLoopStatement","IfStatement","LoopStatement"]};
+window.SIDEBAR_ITEMS = {"enum":["AstControlStatement"],"struct":["CaseStatement","ConditionalBlock","ForLoopStatement","IfStatement","LoopStatement","ReturnStatement"]};
