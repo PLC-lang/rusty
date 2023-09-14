f3440f29fb218483f0e262af61575a85e483532c
mhasel <mhasel@users.noreply.github.com>
2023-09-14 11:37:24 +0000

    deploy: 12b8226b94611dad66036ec01761881664c839eb

--- api/implementors/clap_builder/derive/trait.Subcommand.js
+++ api/implementors/clap_builder/derive/trait.Subcommand.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "clap":[],
 "clap_builder":[],
-"xtask":[["impl <a class=\"trait\" href=\"clap_builder/derive/trait.Subcommand.html\" title=\"trait clap_builder::derive::Subcommand\">Subcommand</a> for <a class=\"enum\" href=\"xtask/enum.Action.html\" title=\"enum xtask::Action\">Action</a>"],["impl <a class=\"trait\" href=\"clap_builder/derive/trait.Subcommand.html\" title=\"trait clap_builder::derive::Subcommand\">Subcommand</a> for <a class=\"enum\" href=\"xtask/enum.Command.html\" title=\"enum xtask::Command\">Command</a>"]]
+"xtask":[["impl <a class=\"trait\" href=\"clap_builder/derive/trait.Subcommand.html\" title=\"trait clap_builder::derive::Subcommand\">Subcommand</a> for <a class=\"enum\" href=\"xtask/enum.Command.html\" title=\"enum xtask::Command\">Command</a>"],["impl <a class=\"trait\" href=\"clap_builder/derive/trait.Subcommand.html\" title=\"trait clap_builder::derive::Subcommand\">Subcommand</a> for <a class=\"enum\" href=\"xtask/enum.Action.html\" title=\"enum xtask::Action\">Action</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
