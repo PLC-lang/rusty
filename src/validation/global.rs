bf79e944667aef74084525f297aa9a934ef16fb7
Michael HASELBERGER <Michael.HASELBERGER@bachmann.info>
2023-12-15 13:09:17 +0100

    no longer validate generic dupes

--- src/validation/global.rs
+++ src/validation/global.rs
@@ -166,22 +166,21 @@ impl GlobalValidator {
             })
             .map(|it| (it.get_name(), it.get_location()));
 
-        let mut map = std::collections::HashMap::new();
         let all_funcs = index
             .get_pous()
             .values()
-            .filter(|p| p.is_function())
-            .filter(|it| {
-                it.get_generic_bindings()
-                    .map(|binding| {
-                        if let Some(duplicate_bindings) = map.insert(it.get_name(), binding) {
-                            duplicate_bindings == binding
-                        } else {
-                            true
-                        }
-                    })
-                    .unwrap_or(true)
-            })
+            .filter(|p| p.is_function() && !p.is_generic())
+            // .filter(|it| {
+            //     it.get_generic_bindings()
+            //         .map(|binding| {
+            //             if let Some(duplicate_bindings) = map.insert(it.get_name(), binding) {
+            //                 duplicate_bindings == binding
+            //             } else {
+            //                 true
+            //             }
+            //         })
+            //         .unwrap_or(true)
+            // })
             .map(|it| (it.get_name(), it.get_location()));
 
         self.check_uniqueness_of_cluster(
@@ -202,15 +201,13 @@ impl GlobalValidator {
             .get_pous()
             .entries()
             .filter(|(_, entries_per_name)| entries_per_name.iter().filter(only_toplevel_pous).count() > 1)
-            .filter(|(_, v)| {
-                v[0].get_generic_bindings()
-                    .map(|bindings| {
-                        v.iter().filter_map(|it| it.get_generic_bindings()).all(|it| bindings == it)
-                    })
-                    .unwrap_or(true)
-            })
             .map(|(name, pous)| {
-                (name.as_str(), pous.iter().filter(only_toplevel_pous).map(|p| p.get_location()))
+                (
+                    name.as_str(),
+                    pous.iter()
+                        .filter(|p| only_toplevel_pous(p) && !p.is_generic())
+                        .map(|p| p.get_location()),
+                )
             });
 
         for (name, cluster) in pou_clusters {
