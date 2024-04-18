66ce86a509eac644191af32f026352414a528ce2
Michael <78988079+mhasel@users.noreply.github.com>
2024-04-18 09:36:52 +0200

    fix: enum-variant names no longer clash (#1188)
    * fix: enum-variant names no longer clash
    ... when codegen runs in parallel.
    
    Enum variant globals are now generated using their qualified name instead
    of their variant name.
    
    Resolves #1182

--- src/codegen/generators/variable_generator.rs
+++ src/codegen/generators/variable_generator.rs
@@ -115,8 +115,13 @@ impl<'ctx, 'b> VariableGenerator<'ctx, 'b> {
         let type_name = global_variable.get_type_name();
         let variable_type = self.types_index.get_associated_type(type_name)?;
 
-        let mut global_ir_variable =
-            self.llvm.create_global_variable(self.module, global_variable.get_name(), variable_type);
+        let name = if self.global_index.get_type_information_or_void(type_name).is_enum() {
+            global_variable.get_qualified_name()
+        } else {
+            global_variable.get_name()
+        };
+
+        let mut global_ir_variable = self.llvm.create_global_variable(self.module, name, variable_type);
         if linkage == LinkageType::External {
             global_ir_variable = global_ir_variable.make_external();
         } else {
