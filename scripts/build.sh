3b239cc5f08dfe38215511acdfa8c1c35030c7e9
Volkan Sagcan <volkanxvs@gmail.com>
2024-06-12 13:33:59 +0200

    Revert "Hardcode test"
    This reverts commit d0cb6900e67a0236ad3e90c5b330b4ef31cbb12c.

--- scripts/build.sh
+++ scripts/build.sh
@@ -146,7 +146,7 @@ function run_lit_test() {
     cargo build
     cargo build --release
 
-    /root/.local/bin/lit -v -DCOMPILER=$project_location/target/debug/plc tests/lit/
+    lit -v -DCOMPILER=$project_location/target/debug/plc tests/lit/
     lit -v -DCOMPILER=$project_location/target/release/plc tests/lit/
 }
 
