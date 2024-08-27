3281887afb0ec8e7b3c019567325d31a6620f9dc
mhasel <mhasel@users.noreply.github.com>
2024-08-27 13:42:55 +0000

    deploy: 41968d32c70eae3eeb4bb3f2589b6efab51419d6

--- api/plc_source/source_location/sidebar-items.js
+++ api/plc_source/source_location/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["CodeSpan"],"struct":["NewLines","SourceLocation","SourceLocationFactory","TextLocation"]};
+window.SIDEBAR_ITEMS = {"enum":["CodeSpan","FileMarker"],"struct":["NewLines","SourceLocation","SourceLocationFactory","TextLocation"]};
--- api/rusty/resolver/sidebar-items.js
+++ api/rusty/resolver/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["AutoDerefType","Dependency","ResolvingScope","StatementAnnotation"],"mod":["const_evaluator","generics"],"struct":["AnnotationMapImpl","AstAnnotations","StringLiterals","TypeAnnotator","VisitorContext"],"trait":["AnnotationMap","Init"],"type":["InitAssignments","Initializers"]};
+window.SIDEBAR_ITEMS = {"enum":["AutoDerefType","Dependency","ResolvingScope","StatementAnnotation"],"mod":["const_evaluator","generics"],"struct":["AnnotationMapImpl","AstAnnotations","StringLiterals","TypeAnnotator","VisitorContext"],"trait":["AnnotationMap"]};
