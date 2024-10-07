4a2d4cbc968993e20cc1fe3b41b37250b0734d93
ghaith <ghaith@users.noreply.github.com>
2024-10-07 07:47:48 +0000

    deploy: 72616b540d46434448eecf2e21812f5b5306e4b2

--- api/insta/sidebar-items.js
+++ api/insta/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"macro":["allow_duplicates","assert_debug_snapshot","assert_display_snapshot","assert_snapshot","with_settings"],"mod":["internals"],"struct":["MetaData","Settings","Snapshot"]};
+window.SIDEBAR_ITEMS = {"macro":["allow_duplicates","assert_compact_debug_snapshot","assert_debug_snapshot","assert_display_snapshot","assert_snapshot","with_settings"],"mod":["internals"],"struct":["MetaData","Settings","Snapshot"]};
