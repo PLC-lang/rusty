f62eba7608ec05e3bdd27ad443e9bf3966e78ed2
Michael Haselberger <Michael.HASELBERGER@bachmann.info>
2025-01-13 13:40:22 +0100

    ignore test on windows due to linker not being available

--- tests/integration/command_line_compile.rs
+++ tests/integration/command_line_compile.rs
@@ -80,6 +80,7 @@ fn stdlib_string_function_headers_compile_to_ir() {
 }
 
 #[test]
+#[cfg_attr(target_os = "windows", ignore = "linker is not available for windows")]
 fn generate_got_file() {
     let file = get_test_file("command_line.st");
 
