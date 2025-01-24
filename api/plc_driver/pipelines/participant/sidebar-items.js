e574d9b7931574a0d89a34e26e0612c13cebb9ed
mhasel <mhasel@users.noreply.github.com>
2025-01-24 10:36:47 +0000

    deploy: 89520b98a42eced03d17c6f042432fda59e0f91e

--- api/plc_driver/pipelines/participant/sidebar-items.js
+++ api/plc_driver/pipelines/participant/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["CodegenParticipant","InitParticipant","LoweringParticipant"],"trait":["PipelineParticipant","PipelineParticipantMut"]};
+window.SIDEBAR_ITEMS = {"struct":["CodegenParticipant","InitParticipant"],"trait":["PipelineParticipant","PipelineParticipantMut"]};
