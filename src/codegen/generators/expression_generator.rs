365b1b4739afb6a905d8cfc7f6fd3f874ce533d5
abroooo <61380142+abroooo@users.noreply.github.com>
2025-04-10 11:59:00 +0200

    cleanup

--- src/codegen/generators/expression_generator.rs
+++ src/codegen/generators/expression_generator.rs
@@ -223,21 +223,12 @@ impl<'ink, 'b> ExpressionCodeGenerator<'ink, 'b> {
                 let function_context = self.function_context.ok_or_else(|| {
                     Diagnostic::codegen_error("Cannot use 'this' without context", expression)
                 })?;
-                let call_name = function_context.linking_context.get_call_name();
-                dbg!(function_context.linking_context.get_type_name());
-                dbg!(&call_name);
-                dbg!(self.annotations.get_call_name(expression));
                 let this_name = &format!("{}.this", self.annotations.get_call_name(expression).unwrap());
-                dbg!(&this_name);
                 let this_value =
                     self.llvm_index.find_loaded_associated_variable_value(&this_name).ok_or_else(|| {
-                        let message = format!(
-                            "Cannot find 'this' for '{}'",
-                            self.annotations.get_call_name(expression).unwrap_or_default()
-                        );
+                        let message = format!("Cannot find '{}' in associated variable values", this_name);
                         Diagnostic::codegen_error(message, expression)
                     })?;
-
                 Ok(ExpressionValue::LValue(this_value))
             }
             AstStatement::ReferenceExpr(data) => {
