0b554559dacb43e835e190ecd9e6faa995165bbc
Angus Bethke <angus.bethke@bachmann.info>
2025-11-13 14:54:57 +0000

    feat: some directory adjustments

--- compiler/plc_header_generator/src/header_generator/template_helper/template_helper_c.rs
+++ compiler/plc_header_generator/src/header_generator/template_helper/template_helper_c.rs
@@ -7,7 +7,7 @@ impl TemplateHelper for GeneratedHeaderForC {
     fn get_template(&self, template_type: TemplateType) -> Template {
         match template_type {
             TemplateType::Header => Template {
-                content: include_str!("templates/header_template.h").to_string(),
+                content: include_str!("templates/c/header_template.h").to_string(),
                 name: String::from("header_template.h"),
             },
         }
