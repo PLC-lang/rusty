5858a6282ecd91ca2d4a061b02da738e9576985f
Michael Haselberger <Michael.HASELBERGER@bachmann.info>
2026-01-19 10:00:03 +0100

    test: update snapshot after llvm update

--- src/codegen/tests/typesystem_test.rs
+++ src/codegen/tests/typesystem_test.rs
@@ -459,26 +459,22 @@ fn arrays_and_strings_passed_as_pointers_in_unsized_variadics() {
     @utf08_literal_0 = private unnamed_addr constant [10 x i8] c"Array: %p\00"
     @utf08_literal_1 = private unnamed_addr constant [11 x i8] c"String: %s\00"
 
-    declare i32 @printf(i8*, ...)
+    declare i32 @printf(ptr, ...)
 
-    define void @main(%main* %0) {
+    define void @main(ptr %0) {
     entry:
       %myString = alloca [81 x i8], align 1
       %myArray = alloca [3 x i16], align 2
-      %1 = bitcast [81 x i8]* %myString to i8*
-      call void @llvm.memcpy.p0i8.p0i8.i64(i8* align 1 %1, i8* align 1 getelementptr inbounds ([81 x i8], [81 x i8]* @__main.myString__init, i32 0, i32 0), i64 ptrtoint ([81 x i8]* getelementptr ([81 x i8], [81 x i8]* null, i32 1) to i64), i1 false)
-      %2 = bitcast [3 x i16]* %myArray to i8*
-      call void @llvm.memcpy.p0i8.p0i8.i64(i8* align 1 %2, i8* align 1 bitcast ([3 x i16]* @__main.myArray__init to i8*), i64 ptrtoint ([3 x i16]* getelementptr ([3 x i16], [3 x i16]* null, i32 1) to i64), i1 false)
-      %3 = bitcast [81 x i8]* %myString to i8*
-      %call = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([11 x i8], [11 x i8]* @utf08_literal_1, i32 0, i32 0), i8* %3)
-      %4 = bitcast [3 x i16]* %myArray to i16*
-      %call1 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([10 x i8], [10 x i8]* @utf08_literal_0, i32 0, i32 0), i16* %4)
+      call void @llvm.memcpy.p0.p0.i64(ptr align 1 %myString, ptr align 1 @__main.myString__init, i64 ptrtoint (ptr getelementptr ([81 x i8], ptr null, i32 1) to i64), i1 false)
+      call void @llvm.memcpy.p0.p0.i64(ptr align 1 %myArray, ptr align 1 @__main.myArray__init, i64 ptrtoint (ptr getelementptr ([3 x i16], ptr null, i32 1) to i64), i1 false)
+      %call = call i32 (ptr, ...) @printf(ptr @utf08_literal_1, ptr %myString)
+      %call1 = call i32 (ptr, ...) @printf(ptr @utf08_literal_0, ptr %myArray)
       ret void
     }
 
-    ; Function Attrs: argmemonly nofree nounwind willreturn
-    declare void @llvm.memcpy.p0i8.p0i8.i64(i8* noalias nocapture writeonly, i8* noalias nocapture readonly, i64, i1 immarg) #0
+    ; Function Attrs: nocallback nofree nounwind willreturn memory(argmem: readwrite)
+    declare void @llvm.memcpy.p0.p0.i64(ptr noalias writeonly captures(none), ptr noalias readonly captures(none), i64, i1 immarg) #0
 
-    attributes #0 = { argmemonly nofree nounwind willreturn }
+    attributes #0 = { nocallback nofree nounwind willreturn memory(argmem: readwrite) }
     "#);
 }
