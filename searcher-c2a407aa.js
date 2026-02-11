57cc6a41ff9b6f1e5d49d585800c1ed86971f57c
Angus-Bethke-Bachmann <Angus-Bethke-Bachmann@users.noreply.github.com>
2026-02-11 13:57:03 +0000

    deploy: 28cfc5a9bd0bd8e7a8e23183e849ba5abcd6ed22

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-153cd98a.js',
+                path_to_root + 'searchindex-c17b38b4.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
