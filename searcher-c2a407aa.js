79cf75d559785166fa9e8d61834ad8c29bb983a7
volsa <volsa@users.noreply.github.com>
2026-03-23 17:47:12 +0000

    deploy: a8d80b5b0203528ad3b3b71b1f303a7a94f8e7e6

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c1da54f3.js',
+                path_to_root + 'searchindex-548805b4.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
