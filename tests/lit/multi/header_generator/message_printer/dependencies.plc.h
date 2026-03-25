55df7b11e2be1b661ee3dd0e17d1491e7c652305
Ghaith Hachem <ghaith.hachem@bachmann.info>
2026-03-25 14:55:24 +0100

    re-track/re-commit the needed files

---
+++ tests/lit/multi/header_generator/message_printer/dependencies.plc.h
@@ -0,0 +1,10 @@
+#ifndef DEPENDENCIES_PLC
+#define DEPENDENCIES_PLC
+
+/*
+ * Add any library dependencies that can not be implicitly detected here
+ */
+#include <stdio.h>
+#include <stdarg.h>
+
+#endif /* !DEPENDENCIES_PLC */
