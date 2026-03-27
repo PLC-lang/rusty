c4cc672e3bfccfaed2762463e19d6511ca2a3ed1
Angus-Bethke-Bachmann <angus.bethke@bachmann.info>
2026-03-27 09:31:50 +0100

    fix: updated call lowering to ensure that output variables are correctly assigned and casted when necessary (#1618)
    * fix: ensuring validation is only altered for the correct context
    
    * fix: lowering type casting for output assignment

--- src/codegen/tests/function_tests.rs
+++ src/codegen/tests/function_tests.rs
@@ -424,3 +424,125 @@ fn function_call_with_array_access() {
 
     filtered_assert_snapshot!(result)
 }
+
+#[test]
+fn function_output_should_be_cast_if_needed() {
+    let result = codegen(
+        "
+        FUNCTION libFunction : INT
+        VAR_INPUT
+            inVar1 : INT;
+            inVar2 : REAL;
+        END_VAR
+        VAR_OUTPUT
+            result : REAL;
+        END_VAR
+        END_FUNCTION
+
+        PROGRAM mainProg
+            VAR
+                i1 : INT;
+            END_VAR
+
+            libFunction(
+                inVar1 := 0,
+                inVar2 := 0.0,
+                result => i1
+            );
+        END_PROGRAM
+       ",
+    );
+
+    filtered_assert_snapshot!(result, @r#"
+    ; ModuleID = '<internal>'
+    source_filename = "<internal>"
+    target datalayout = "[filtered]"
+    target triple = "[filtered]"
+
+    %mainProg = type { i16 }
+
+    @mainProg_instance = global %mainProg zeroinitializer
+
+    define i16 @libFunction(i16 %0, float %1, ptr %2) {
+    entry:
+      %libFunction = alloca i16, align [filtered]
+      %inVar1 = alloca i16, align [filtered]
+      store i16 %0, ptr %inVar1, align [filtered]
+      %inVar2 = alloca float, align [filtered]
+      store float %1, ptr %inVar2, align [filtered]
+      %result = alloca ptr, align [filtered]
+      store ptr %2, ptr %result, align [filtered]
+      store i16 0, ptr %libFunction, align [filtered]
+      %libFunction_ret = load i16, ptr %libFunction, align [filtered]
+      ret i16 %libFunction_ret
+    }
+
+    define void @mainProg(ptr %0) {
+    entry:
+      %i1 = getelementptr inbounds nuw %mainProg, ptr %0, i32 0, i32 0
+      %__libFunction_result0 = alloca float, align [filtered]
+      store float 0.000000e+00, ptr %__libFunction_result0, align [filtered]
+      %call = call i16 @libFunction(i16 0, float 0.000000e+00, ptr %__libFunction_result0)
+      %load___libFunction_result0 = load float, ptr %__libFunction_result0, align [filtered]
+      %1 = fptosi float %load___libFunction_result0 to i16
+      store i16 %1, ptr %i1, align [filtered]
+      ret void
+    }
+    "#)
+}
+
+#[test]
+fn function_block_output_should_be_cast_if_needed() {
+    let result = codegen(
+        "
+        FUNCTION_BLOCK libFunction_fb
+        VAR_OUTPUT
+            result : REAL;
+        END_VAR
+        END_FUNCTION_BLOCK
+
+        PROGRAM mainProg
+            VAR
+                libFunction : libFunction_fb;
+                i1 : INT;
+            END_VAR
+
+            libFunction(
+                result => i1
+            );
+        END_PROGRAM
+       ",
+    );
+
+    filtered_assert_snapshot!(result, @r#"
+    ; ModuleID = '<internal>'
+    source_filename = "<internal>"
+    target datalayout = "[filtered]"
+    target triple = "[filtered]"
+
+    %mainProg = type { %libFunction_fb, i16 }
+    %libFunction_fb = type { float }
+
+    @mainProg_instance = global %mainProg zeroinitializer
+
+    define void @libFunction_fb(ptr %0) {
+    entry:
+      %this = alloca ptr, align [filtered]
+      store ptr %0, ptr %this, align [filtered]
+      %result = getelementptr inbounds nuw %libFunction_fb, ptr %0, i32 0, i32 0
+      ret void
+    }
+
+    define void @mainProg(ptr %0) {
+    entry:
+      %libFunction = getelementptr inbounds nuw %mainProg, ptr %0, i32 0, i32 0
+      %i1 = getelementptr inbounds nuw %mainProg, ptr %0, i32 0, i32 1
+      call void @libFunction_fb(ptr %libFunction)
+      %1 = getelementptr inbounds %libFunction_fb, ptr %libFunction, i32 0, i32 0
+      %2 = load float, ptr %1, align [filtered]
+      %3 = fptosi float %2 to i16
+      store i16 %3, ptr %i1, align [filtered]
+      ret void
+    }
+    "#)
+}
